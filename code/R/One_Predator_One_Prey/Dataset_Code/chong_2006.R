50fa6c952b2f1b42b83a5c6415f4af284ba35d2c
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2018-08-01 13:03:23 +1200

    Chong 2006 data file was renamed

--- code/R/One_Predator_One_Prey/Dataset_Code/chong_2006.R
+++ code/R/One_Predator_One_Prey/Dataset_Code/chong_2006.R
@@ -1,7 +1,7 @@
 
 # read in the simplify the raw data
 datadir <- 'Chong_2006'
-filename <- 'Chong_2006_expmt2.csv'
+filename <- 'Chong_2006.csv'
 rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
 
 # rename to standard column names used in fitting code
