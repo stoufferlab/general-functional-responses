dce33a9aeb72505d02db9c021451a603b0ca00a8
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2018-08-01 14:07:09 +1200

    removing broken files (which can be recreated later) and renaming the template code to make it clearer what they correspond to

--- code/R/One_Predator_One_Prey/Dataset_Code/zzz_mills_2004.R
+++
@@ -1,9 +0,0 @@
-
-# read in the simplify the raw data
-datadir <- 'Mills_2004'
-filename <- 'Mills_2004.csv'
-rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
-
-# rename to standard column names used in fitting code
-d <- rawdata[,c("Parasitoids", "Hosts", "TotalParasitized")]
-colnames(d) <- c("Npredator", "Nprey", "Nconsumed")
--- code/R/One_Predator_One_Prey/Dataset_Code/zzz_stier_2014.R
+++
@@ -1,9 +0,0 @@
-
-# read in the simplify the raw data
-datadir <- 'Stier_2014'
-filename <- 'Stier_2014.csv'
-rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
-
-# rename to standard column names used in fitting code
-d <- rawdata[,c("pred", "total.settlers", "dead")]
-colnames(d) <- c("Npredator", "Nprey", "Nconsumed")
