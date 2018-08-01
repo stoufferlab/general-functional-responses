7379bb6d69adc909541d6fa6b4e86bbbce877952
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2018-08-01 13:22:57 +1200

    Reeve 1997 data file was renamed as was the column header

--- code/R/One_Predator_One_Prey/Dataset_Code/reeve_1997.R
+++ code/R/One_Predator_One_Prey/Dataset_Code/reeve_1997.R
@@ -1,11 +1,11 @@
 
 # read in the simplify the raw data
 datadir <- 'Reeve_1997'
-filename <- 'Tdubius_functional_response.csv'
+filename <- 'Reeve_1997.csv'
 rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
 
 # rename to standard column names used in fitting code
-d <- rawdata[,c("n", "p", "na")]
+d <- rawdata[,c("Prey", "Pred", "Eaten")]
 colnames(d) <- c("Nprey", "Npredator", "Nconsumed")
 
 # WARNING non-integers and NA values muck things up
