70bd815549943a0fab2099d7f2a0642f55a656e8
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2018-08-02 16:34:57 +1200

    made a single negative log likelihood function that works for nloptr and mle2

--- code/R/LogLikelihoods/holling_like_one_predator_one_prey_unbounded.R
+++ code/R/LogLikelihoods/holling_like_one_predator_one_prey_unbounded.R
@@ -18,10 +18,14 @@ holling.like.1pred.1prey = function(N0, a, h, c, phi_numer, phi_denom, P, T, exp
 	}
 
 	if(expttype=="integrated"){
-		h <- h * (1 + (1 - phi_numer * phi_denom) * a * h * c * P_interfering)
-		Q <- (1 + c * P_interfering)
-		X <- (1 + (1 - phi_numer) * c * P_interfering)
-		N <- N0 - (Q / (a * h)) * lamW::lambertW0(((a * h * N0)/ Q) * exp(- (a / Q) * (X * P * T - h * N0)))
+		if(h==0){
+			N <- N0 * (1 - exp(-a * P * T))
+		}else{
+			h <- h * (1 + (1 - phi_numer * phi_denom) * a * h * c * P_interfering)
+			Q <- (1 + c * P_interfering)
+			X <- (1 + (1 - phi_numer) * c * P_interfering)
+			N <- N0 - (Q / (a * h)) * lamW::lambertW0(((a * h * N0)/ Q) * exp(- (a / Q) * (X * P * T - h * N0)))
+		}
 	}
 
 	if(expttype=="replacement"){
@@ -33,38 +37,123 @@ holling.like.1pred.1prey = function(N0, a, h, c, phi_numer, phi_denom, P, T, exp
 	return(N)
 }
 
-# negative log likelihood for data predicted by a ratio-dependent-like functional response
-holling.like.1pred.1prey.NLL = function(
-	attack,
-	handling,
-	interference,
-	phi_numer,
-	phi_denom,
-	initial,
-	killed,
-	predators,
-	expttype,
-	Pminus1,
-	# phi_link=c('identity', 'logit'),
-	time=NULL
-){
-	# phi_link <- match.arg(phi_link)
-	# DEBUG we should probably force some of these here depending on model type instead of (below)
+# # negative log likelihood for data predicted by a ratio-dependent-like functional response
+# holling.like.1pred.1prey.NLL = function(
+# 	attack,
+# 	handling,
+# 	interference,
+# 	phi_numer,
+# 	phi_denom,
+# 	initial,
+# 	killed,
+# 	predators,
+# 	expttype,
+# 	Pminus1,
+# 	# phi_link=c('identity', 'logit'),
+# 	time=NULL
+# ){
+# 	# phi_link <- match.arg(phi_link)
+# 	# DEBUG we should probably force some of these here depending on model type instead of (below)
 
-	# we use parameter transformations to help improve the fitting and to avoid needing bounded optimization
-	attack <- exp(attack)
-	handling <- exp(handling)
-	interference <- exp(interference)
+# 	# we use parameter transformations to help improve the fitting and to avoid needing bounded optimization
+# 	attack <- exp(attack)
+# 	handling <- exp(handling)
+# 	interference <- exp(interference)
 
-	# # at times we will constrain these parameters to be [0,1]
-	# phi_numer <- switch(phi_link,
-	# 	identity = identity(phi_numer),
-	# 	logit = plogis(phi_numer)
-	# )
-	# phi_denom <- switch(phi_link,
-	# 	identity = identity(phi_denom),
-	# 	logit = plogis(phi_denom)
-	# )
+# 	# # at times we will constrain these parameters to be [0,1]
+# 	# phi_numer <- switch(phi_link,
+# 	# 	identity = identity(phi_numer),
+# 	# 	logit = plogis(phi_numer)
+# 	# )
+# 	# phi_denom <- switch(phi_link,
+# 	# 	identity = identity(phi_denom),
+# 	# 	logit = plogis(phi_denom)
+# 	# )
+
+# 	# if no times are specified then normalize to time=1
+# 	if(is.null(time)){
+# 		time <- 1
+# 	}
+
+# 	# expected number consumed given data and parameters
+# 	Nconsumed <- holling.like.1pred.1prey(N0=initial, a=attack, h=handling, c=interference, phi_numer=phi_numer, phi_denom=phi_denom, P=predators, T=time, expttype=expttype, Pminus1=Pminus1)
+
+# 	# DEBUG if the parameters are not biologically plausible, neither should be the likelihood
+# 	if(any(Nconsumed < 0 | is.nan(Nconsumed))){
+# 		return(Inf)
+# 	}
+
+# 	# negative log likelihood based on proportion consumed (no replacement)
+# 	# DEBUG: consider whether binomial or poisson are interchangeable
+# 	if(expttype=="integrated"){
+# 		nll <- -sum(dbinom(killed, prob=Nconsumed/initial, size=initial, log=TRUE))
+# 	}
+
+# 	# negative log likelihood based on total number consumed (replacement)
+# 	if(expttype=="replacement"){
+# 		nll <- -sum(dpois(killed, Nconsumed, log=TRUE))
+# 	}
+
+# 	return(nll)
+# }
+
+# convenience function for these holling-like models that will allow the use of nloptr instead of mle2
+holling.like.1pred.1prey.NLL = function(params, modeltype, initial, killed, predators, expttype, Pminus1, time=NULL){
+	if(modeltype == "Holling I"){
+		attack <- exp(params[1])
+		handling <- 0
+		interference <- 0
+		phi_numer <- 1
+		phi_denom <- 1
+	}
+
+	if(modeltype == "Holling II"){
+		attack <- exp(params[1])
+		handling <- exp(params[2])
+		interference <- 0
+		phi_numer <- 1
+		phi_denom <- 1
+	}
+
+	if(modeltype == "Beddington-DeAngelis"){
+		attack <- exp(params[1])
+		handling <- exp(params[2])
+		interference <- exp(params[3])
+		phi_numer <- 1
+		phi_denom <- 1
+	}
+
+	if(modeltype == "Crowley-Martin"){
+		attack <- exp(params[1])
+		handling <- exp(params[2])
+		interference <- exp(params[3])
+		phi_numer <- 1
+		phi_denom <- 0
+	}
+
+	if(modeltype == "Stouffer-Novak I"){
+		attack <- exp(params[1])
+		handling <- exp(params[2])
+		interference <- exp(params[3])
+		phi_numer <- 1
+		phi_denom <- params[4]
+	}
+
+	if(modeltype == "Stouffer-Novak II"){
+		attack <- exp(params[1])
+		handling <- exp(params[2])
+		interference <- exp(params[3])
+		phi_numer <- params[4]
+		phi_denom <- 1
+	}
+	
+	if(modeltype == "Stouffer-Novak III"){
+		attack <- exp(params[1])
+		handling <- exp(params[2])
+		interference <- exp(params[3])
+		phi_numer <- params[4]
+		phi_denom <- params[5]
+	}
 
 	# if no times are specified then normalize to time=1
 	if(is.null(time)){
@@ -80,7 +169,6 @@ holling.like.1pred.1prey.NLL = function(
 	}
 
 	# negative log likelihood based on proportion consumed (no replacement)
-	# DEBUG: consider whether binomial or poisson are interchangeable
 	if(expttype=="integrated"){
 		nll <- -sum(dbinom(killed, prob=Nconsumed/initial, size=initial, log=TRUE))
 	}
@@ -93,64 +181,11 @@ holling.like.1pred.1prey.NLL = function(
 	return(nll)
 }
 
-# convenience function for these holling-like models that will allow the use of nloptr instead of mle2
-holling.like.1pred.1prey.NLL.params = function(params, modeltype, initial, killed, predators, expttype, Pminus1, time=NULL){
-	if(modeltype == "Holling I"){
-		attack <- params[1]
-		handling <- log(1E-7)
-		interference <- log(1E-7)
-		phi_numer <- 1
-		phi_denom <- 1
-	}
-
-	if(modeltype == "Holling II"){
-		attack <- params[1]
-		handling <- params[2]
-		interference <- log(1E-7)
-		phi_numer <- 1
-		phi_denom <- 1
-	}
-
-	if(modeltype == "Beddington-DeAngelis"){
-		attack <- params[1]
-		handling <- params[2]
-		interference <- params[3]
-		phi_numer <- 1
-		phi_denom <- 1
-	}
-
-	if(modeltype == "Crowley-Martin"){
-		attack <- params[1]
-		handling <- params[2]
-		interference <- params[3]
-		phi_numer <- 1
-		phi_denom <- 0
-	}
-
-	if(modeltype == "Stouffer-Novak I"){
-		attack <- params[1]
-		handling <- params[2]
-		interference <- params[3]
-		phi_numer <- 1
-		phi_denom <- params[4]
-	}
-
-	if(modeltype == "Stouffer-Novak II"){
-		attack <- params[1]
-		handling <- params[2]
-		interference <- params[3]
-		phi_numer <- params[4]
-		phi_denom <- 1
-	}
-	
-	if(modeltype == "Stouffer-Novak III"){
-		attack <- params[1]
-		handling <- params[2]
-		interference <- params[3]
-		phi_numer <- params[4]
-		phi_denom <- params[5]
-	}
-
-	nll <- holling.like.1pred.1prey.NLL(attack=attack, handling=handling, interference=interference, phi_numer=phi_numer, phi_denom=phi_denom, initial=initial, killed=killed, predators=predators, time=time, expttype=expttype, Pminus1=Pminus1)
-	return(nll)
-}
+# DEBUG there must be a more elegant way to do this "within" the function itself
+parnames(holling.like.1pred.1prey.NLL) <- c(
+	'attack',
+	'handling',
+	'interference',
+	'phi_numer',
+	'phi_denom'
+)
--- code/R/One_Predator_One_Prey/fit_holling_like_nobounds.R
+++ code/R/One_Predator_One_Prey/fit_holling_like_nobounds.R
@@ -16,7 +16,7 @@ source('../LogLikelihoods/holling_like_one_predator_one_prey_unbounded.R')
 
 ffr.hollingI.nloptr <- nloptr::nloptr(
 	x0=x0.hl,
-	eval_f=holling.like.1pred.1prey.NLL.params,
+	eval_f=holling.like.1pred.1prey.NLL,
 	opts=list(print_level=0, algorithm='NLOPT_LN_SBPLX', maxeval=1E5),
 	modeltype="Holling I",
 	initial=d$Nprey,
@@ -29,14 +29,23 @@ ffr.hollingI.nloptr <- nloptr::nloptr(
 
 ffr.hollingI <- bbmle::mle2(
 	holling.like.1pred.1prey.NLL,
+	vecpar=TRUE,
 	start=list(attack=ffr.hollingI.nloptr$solution[1]),
 	fixed=list(
-		handling = log(1E-7),
-		interference = log(1E-7),
+		handling = 0,
+		interference = 0,
 		phi_numer = 1,
 		phi_denom = 1
 	),
-	data=list(initial=d$Nprey, killed=d$Nconsumed, predators=d$Npredator, time=d$Time, expttype=expttype, Pminus1=Pminus1),
+	data=list(
+		modeltype="Holling I",
+		initial=d$Nprey,
+		killed=d$Nconsumed,
+		predators=d$Npredator,
+		time=d$Time,
+		expttype=expttype,
+		Pminus1=Pminus1
+	)
 	#skip.hessian = TRUE
 	# optimizer="nlminb",
 	# lower=c(attack=0, handling=0)
@@ -44,7 +53,7 @@ ffr.hollingI <- bbmle::mle2(
 
 ffr.hollingII.nloptr <- nloptr::nloptr(
 	x0=c(coef(ffr.hollingI)[1],log(1)),
-	eval_f=holling.like.1pred.1prey.NLL.params,
+	eval_f=holling.like.1pred.1prey.NLL,
 	opts=list(print_level=0, algorithm='NLOPT_LN_SBPLX', maxeval=1E5),
 	modeltype="Holling II",
 	initial=d$Nprey,
@@ -57,13 +66,17 @@ ffr.hollingII.nloptr <- nloptr::nloptr(
 
 ffr.hollingII <- bbmle::mle2(
 	holling.like.1pred.1prey.NLL,
-	start=list(attack=ffr.hollingII.nloptr$solution[1], handling=ffr.hollingII.nloptr$solution[2]),
+	vecpar=TRUE,
+	start=list(
+		attack=ffr.hollingII.nloptr$solution[1],
+		handling=ffr.hollingII.nloptr$solution[2]
+	),
 	fixed=list(
-		interference = log(1E-7),
+		interference = 0,
 		phi_numer = 1,
 		phi_denom = 1
 	),
-	data=list(initial=d$Nprey, killed=d$Nconsumed, predators=d$Npredator, time=d$Time, expttype=expttype, Pminus1=Pminus1, params=NULL)
+	data=list(modeltype="Holling II", initial=d$Nprey, killed=d$Nconsumed, predators=d$Npredator, time=d$Time, expttype=expttype, Pminus1=Pminus1)
 	#skip.hessian = TRUE
 	# optimizer="nlminb",
 	# lower=c(attack=0, handling=0)
@@ -123,7 +136,7 @@ ffr.hollingII <- bbmle::mle2(
 
 ffr.sn1.nloptr <- nloptr::nloptr(
 	x0=c(coef(ffr.hollingII)[1:2],log(1),0.5),
-	eval_f=holling.like.1pred.1prey.NLL.params,
+	eval_f=holling.like.1pred.1prey.NLL,
 	opts=list(print_level=0, algorithm='NLOPT_LN_SBPLX', maxeval=1E5),
 	modeltype="Stouffer-Novak I",
 	initial=d$Nprey,
@@ -135,17 +148,31 @@ ffr.sn1.nloptr <- nloptr::nloptr(
 )
 
 # WARNING THIS IS FOR TESTING ONLY
-ffr.sn1.nloptr$solution <- c(coef(ffr.hollingII)[1:2],log(1),0.5)
+# ffr.sn1.nloptr$solution <- c(coef(ffr.hollingII)[1:2],log(1),0.5)
 
 # use mle2 because it has handy machinery
 
 ffr.sn1 <- bbmle::mle2(
 	holling.like.1pred.1prey.NLL,
-	start=list(attack=ffr.sn1.nloptr$solution[1], handling=ffr.sn1.nloptr$solution[2], interference=ffr.sn1.nloptr$solution[3], phi_denom=ffr.sn1.nloptr$solution[4]),
+	vecpar=TRUE,
+	start=list(
+		attack=ffr.sn1.nloptr$solution[1],
+		handling=ffr.sn1.nloptr$solution[2],
+		interference=ffr.sn1.nloptr$solution[3],
+		phi_denom=ffr.sn1.nloptr$solution[4]
+	),
 	fixed=list(
 		phi_numer = 1
 	),
-	data=list(initial=d$Nprey, killed=d$Nconsumed, predators=d$Npredator, time=d$Time, expttype=expttype, Pminus1=Pminus1),
+	data=list(
+		modeltype="Stouffer-Novak I",
+		initial=d$Nprey,
+		killed=d$Nconsumed,
+		predators=d$Npredator,
+		time=d$Time,
+		expttype=expttype,
+		Pminus1=Pminus1
+	),
 	skip.hessian = TRUE
 )
 
@@ -153,6 +180,7 @@ ffr.sn1 <- bbmle::mle2(
 
 ffr.sn1 <- bbmle::mle2(
 	holling.like.1pred.1prey.NLL,
+	vecpar=TRUE,
 	start=list(
 		attack=coef(ffr.sn1)[1],
 		handling=coef(ffr.sn1)[2],
@@ -163,6 +191,7 @@ ffr.sn1 <- bbmle::mle2(
 		phi_numer = 1
 	),
 	data=list(
+		modeltype="Stouffer-Novak I",
 		initial=d$Nprey,
 		killed=d$Nconsumed,
 		predators=d$Npredator,
