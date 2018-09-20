a79529e583682d7ac2c326673fba71f95a4c21b7
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2018-09-20 15:16:54 +1200

    this data goes unused for now

--- code/R/One_Predator_One_Prey/Dataset_Code/ganjisaffar_2017.R
+++
@@ -1,9 +0,0 @@
-
-# read in the simplify the raw data
-datadir <- 'Ganjisaffar_2017'
-filename <- 'Ganjisaffar_2017.csv'
-rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
-
-# rename to standard column names used in fitting code
-d <- rawdata[,c('Predator.Density','Prey.Density','Eaten')]
-colnames(d) <- c("Npredator", "Nprey", "Nconsumed")
