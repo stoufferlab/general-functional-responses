f86083a315d2b93a39886540c6279e3acd852a78
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2018-10-05 15:11:41 +1300

    Colton 1987 processing codes

---
+++ code/R/One_Predator_Two_Prey/Dataset_Code/Colton_1987_1st24.R
@@ -0,0 +1,9 @@
+
+# read in the simplify the raw data
+datadir <- 'Colton_1987'
+filename <- 'Colton_1987_1st24.csv'
+rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
+
+# rename to standard column names used in fitting code
+d <- rawdata[,c('Pred','Prey1.SIN','Prey1.Eaten','Prey2.DIN','Prey2.Eaten')]
+colnames(d) <- c("Npredator", "Nprey1", "Nconsumed1", "Nprey2", "Nconsumed2")
