b624c619decab502e7c13d2e1c7c4b12a8af874a
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2018-10-05 15:11:15 +1300

    Kalinkat 2011 processing codes

---
+++ code/R/One_Predator_Two_Prey/Dataset_Code/Kalinkat_2011_Pard.R
@@ -0,0 +1,10 @@
+
+# read in the simplify the raw data
+datadir <- 'Kalinkat_2011'
+filename <- 'Kalinkat_2011_Pard.csv'
+rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
+
+# rename to standard column names used in fitting code
+d <- rawdata[,c('large_prey_N0','large_prey_NE','small_prey_N0','small_prey_NE')]
+colnames(d) <- c("Nprey1", "Nconsumed1", "Nprey2", "Nconsumed2")
+d$Npredator <- 1
