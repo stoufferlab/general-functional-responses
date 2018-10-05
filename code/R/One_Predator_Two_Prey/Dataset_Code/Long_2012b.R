07fc067593ccb11454a26a9729d6e459d768b2d8
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2018-10-05 15:11:59 +1300

    Long 2012b processing code

---
+++ code/R/One_Predator_Two_Prey/Dataset_Code/Long_2012b.R
@@ -0,0 +1,9 @@
+
+# read in the simplify the raw data
+datadir <- 'Long_2012b'
+filename <- 'Long_2012b.csv'
+rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
+
+# rename to standard column names used in fitting code
+d <- rawdata[,c('Pred','Prey1.Cr','Prey1.Eaten','Prey2.Cl','Prey2.Eaten')]
+colnames(d) <- c("Npredator", "Nprey1", "Nconsumed1", "Nprey2", "Nconsumed2")
