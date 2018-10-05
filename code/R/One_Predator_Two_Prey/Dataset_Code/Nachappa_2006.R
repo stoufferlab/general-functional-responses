6db99a907d1f32bc7e17e2bd2365dbf934fbcc3e
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2018-10-05 15:11:00 +1300

    proper Nachappa 2006 processing code

---
+++ code/R/One_Predator_Two_Prey/Dataset_Code/Nachappa_2006.R
@@ -0,0 +1,9 @@
+
+# read in the simplify the raw data
+datadir <- 'Nachappa_2006'
+filename <- 'Nachappa_2006.csv'
+rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
+
+# rename to standard column names used in fitting code
+d <- rawdata[,c('Pred','Prey1.TLS','Prey1.Eaten','Prey2.FAW','Prey2.Eaten')]
+colnames(d) <- c("Npredator", "Nprey1", "Nconsumed1", "Nprey2", "Nconsumed2")
