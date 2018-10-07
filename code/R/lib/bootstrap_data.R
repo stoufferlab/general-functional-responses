3df4377fcf4a2c780795dc98cde268da72df5c37
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2018-10-08 09:09:51 +1300

    the bootstrap function now works for an arbitrary set of prey so long as the input dataframe has appropriately named column headers

--- code/R/lib/bootstrap_data.R
+++ code/R/lib/bootstrap_data.R
@@ -1,60 +1,136 @@
 
-# DEBUG needs a concrete SE estimate and actual sample sizes
+# generate boostrapped data from a set of means and SEs of experimental observations
 bootstrap.data <- function(rawdata, expttype=c("integrated","replacement")){
+	# make sure that the expttype variable is valid
 	expttype <- match.arg(expttype)
+
+	# determine the "names" of all prey present
+	prey <- grep("[.]mean$", colnames(rawdata), value=TRUE)
+	prey <- gsub("[.]mean$", "", prey)
+	prey <- gsub("^Nconsumed", "", prey)
 
 	# generate a container for the data
 	if("Time" %in% colnames(rawdata)){
-		d <- matrix(NA, 0, 4)
+		d <- matrix(NA, 0, 2 + 2 * length(prey))
 	}else{
-		d <- matrix(NA, 0, 3)
+		d <- matrix(NA, 0, 1 + 2 * length(prey))
 	}
 
-	
-
-	# bootstrap
-	counter <- 1	
+	# go row by row through the data
 	for(r in 1:nrow(rawdata)){
-		# DEBUG: if the SE is NA then do not sample that set of replicates more than once
-		if(is.na(rawdata[r,"Nconsumed.se"])){
-			rawdata[r,"Nconsumed.se"] <- 0
+		# if for some reason we do not know the SE for any of the different observations treat whole set as a SINGLE replicate
+		if(any(is.na(rawdata[r,paste0("Nconsumed",prey,".se")]))){
+			rawdata[r,paste0("Nconsumed",prey,".se")] <- 0
 			rawdata[r,"n"] <- 1
 		}
-		
-		for( e in 1:rawdata[r,"n"] ){
-			mu <- rawdata[r,"Nconsumed.mean"]
-			sigma <- rawdata[r,"Nconsumed.se"] * sqrt(rawdata[r,"n"])
 
-			if(expttype=="integrated"){
-				prob <- rnorm(n=1, mean=mu, sd=sigma) / rawdata[r,"Nprey"]
-				prob <- max(0, min(1, prob))
-				Ne <- rbinom(
-						n=1,
-						size=rawdata[r,"Nprey"],
-						p=prob
+		# go replicate by replicate
+		for(e in 1:rawdata[r,"n"] ){
+			Na <- c()
+			Ne <- c()
+			# sample prey by prey
+			for(i in prey){
+				# known number of available prey
+				Na <- c(
+					Na, rawdata[r,paste0("Nprey",i)]
+				)
+
+				# sample number of eaten prey
+				Ne <- c(
+					Ne,
+					fr.sample(
+						mean=rawdata[r,paste0("Nconsumed",i,".mean")],
+						se=rawdata[r,paste0("Nconsumed",i,".se")],
+						n=rawdata[r,"n"],
+						expttype=expttype,
+						Nprey=rawdata[r,paste0("Nprey",i)]
+					)
 				)
 			}
 
-			if(expttype=="replacement"){
-				lambda <- rnorm(n=1, mean=mu, sd=sigma)
-				lambda <- max(0, lambda)
-				Ne <- rpois(n=1, lambda=lambda)
+			# add the requisite infoto the new row
+			new.row <- c(
+				rawdata[r,"Npredator"],
+				Na,
+				Ne
+			)
+			
+			# if there was time/duration data in the original data frame, put it back in
+			if("Time" %in% colnames(rawdata)){
+				new.row <- c(
+					new.row,
+					rawdata[r,"Time"]
+				)
 			}
 
-			if("Time" %in% colnames(rawdata)){
-				d <- rbind(d, c(rawdata[r,"Npredator"],rawdata[r,"Nprey"],Ne,rawdata[r,"Time"]))
-			}else{
-				d <- rbind(d, c(rawdata[r,"Npredator"],rawdata[r,"Nprey"],Ne))
-			}
+			# add the new "observation" to the data set
+			d <- rbind(d, new.row)
 		}
 	}
-	
-	d <- data.frame(d)
+
+	# all data frames should emerge from this function with identical column names
+	d <- data.frame(d, row.names=seq(1,nrow(d)))
 	if("Time" %in% colnames(rawdata)){
-		colnames(d) <- c("Npredator", "Nprey", "Nconsumed", "Time")
+		colnames(d) <- c(
+			"Npredator",
+			paste0("Nprey", prey),
+			paste0("Nconsumed", prey),
+			"Time"
+		)
 	}else{
-		colnames(d) <- c("Npredator", "Nprey", "Nconsumed")
+		colnames(d) <- c(
+			"Npredator",
+			paste0("Nprey", prey),
+			paste0("Nconsumed", prey)
+		)
 	}
 
 	return(d)
+}
+
+# sample a number consumed given mean, se, n, and Nprey (which is necessary for non-replacement studies)
+fr.sample <- function(mean, se, n, expttype=c("integrated", "replacement"), Nprey=NULL){
+	# make sure that the expttype variable is valid
+	expttype <- match.arg(expttype)
+
+	# we require Nprey for non-replacement since they are proportion of a total which must be specified
+	if(is.null(Nprey) && expttype == "integrated"){
+		stop("fr.sample requires Nprey argument for non-replacement experiment types")
+	}
+
+	# normal distribution nomenclature
+	mu <- mean
+	sigma <- se * sqrt(n)
+
+	if(expttype=="integrated"){
+		# calculate the proportion of prey consumed; do not divide by zero prey since no prey available means no prey consumed!
+		prob <- ifelse(
+			Nprey==0,
+			0,
+			rnorm(n=1, mean=mu, sd=sigma) / Nprey
+		)
+
+		# make sure we end up with a valid proportion
+		prob <- max(0, min(1, prob))
+		
+		# bootstrapped number eaten is the result of a binomial process
+		Ne <- rbinom(
+				n=1,
+				size=Nprey,
+				p=prob
+		)
+	}
+
+	if(expttype=="replacement"){
+		# calculate the expected number consumed given the rate of consumption
+		lambda <- rnorm(n=1, mean=mu, sd=sigma)
+
+		# make sure we end up with a valid rate (>=0)
+		lambda <- max(0, lambda)
+
+		# boostrapped number eaten is the result of a poisson process
+		Ne <- rpois(n=1, lambda=lambda)
+	}
+
+	return(Ne)
 }
