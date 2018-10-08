f037af26565a84aa6933eb3b629deeaf0f62fe2b
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2018-10-09 08:38:24 +1300

    removing the check on non-integer values from Reeve 1997 since this is a bigger fish to fry elsewhere

--- code/R/One_Predator_One_Prey/Dataset_Code/Reeve_1997.R
+++ code/R/One_Predator_One_Prey/Dataset_Code/Reeve_1997.R
@@ -7,7 +7,3 @@ rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
 # rename to standard column names used in fitting code
 d <- rawdata[,c("Prey", "Pred", "Eaten")]
 colnames(d) <- c("Nprey", "Npredator", "Nconsumed")
-
-# WARNING non-integers and NA values muck things up
-d <- d[!is.na(d$Nconsumed),]
-d$Nconsumed <- round(d$Nconsumed)
