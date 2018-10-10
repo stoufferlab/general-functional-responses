349a58cd1eaf68de2feec02e923be09c3ff6ca51
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2018-10-10 15:10:55 +1300

    adding Iyer 1996 scripts; had not realized there are three prey species but every experiment was for pairs...

---
+++ code/R/One_Predator_Two_Prey/Dataset_Code/Iyer_1996_Br.R
@@ -0,0 +1,9 @@
+
+# read in the simplify the raw data
+datadir <- 'Iyer_1996'
+filename <- 'Iyer_1996_Br.csv'
+rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
+
+# rename to standard column names used in fitting code
+d <- rawdata[,c('Preds','Prey1.Br','Prey1Eaten.Mean','Prey1Eaten.SE','Prey2.Hm','Prey2Eaten.Mean','Prey2Eaten.SE','n','Time')]
+colnames(d) <- c("Npredator", "Nprey1", "Nconsumed1.mean", "Nconsumed1.se", "Nprey2", "Nconsumed2.mean", "Nconsumed2.se", "n", "Time")
