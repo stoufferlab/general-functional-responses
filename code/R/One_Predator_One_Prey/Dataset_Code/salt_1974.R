dd3c9f6233ba98329ece174f7404079d4931603e
Mark Novak <mnovak.m@gmail.com>
2018-10-16 11:33:18 -0700

    Update to Salt 74 script

--- code/R/One_Predator_One_Prey/Dataset_Code/salt_1974.R
+++ code/R/One_Predator_One_Prey/Dataset_Code/salt_1974.R
@@ -5,5 +5,5 @@ filename <- 'Salt_1974.csv'
 rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
 
 # turn into a standard dataframe with standard column names
-d <- rawdata[,c('Preds','Prey','FeedingRate.Total','FeedingRate.Total.SE','n')]
-colnames(d) <- c("Npredator", "Nprey", "Nconsumed.mean", "Nconsumed.se", "n")
+d <- rawdata[,c('Preds','Prey','FeedingRate.Total','FeedingRate.Total.SE','n','Time')]
+colnames(d) <- c("Npredator", "Nprey", "Nconsumed.mean", "Nconsumed.se", "n",'Time')
