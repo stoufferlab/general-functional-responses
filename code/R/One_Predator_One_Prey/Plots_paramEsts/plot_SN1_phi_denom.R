17e8450b0fbc4045d5294bf881d2a764cb426b19
Mark Novak <mnovak.m@gmail.com>
2019-03-05 08:07:56 -0800

    folder move

--- code/R/One_Predator_One_Prey/Plots_paramEsts/plot_SN1_phi_denom.R
+++ code/R/One_Predator_One_Prey/Plots_paramEsts/plot_SN1_phi_denom.R
@@ -5,7 +5,7 @@ source('../../lib/ratio_method_one_predator_one_prey.R')
 load('../../../../results/R/ffr.fits_OnePredOnePrey.Rdata')
 
 
-fit.order <- order.of.fits(ffr.fits, model="Stouffer.Novak.I", order.parm="Sample size", order=TRUE, point.est='median')
+fit.order <- order.of.fits(ffr.fits, model="Stouffer.Novak.I", order.parm="phi_denom", order=TRUE, point.est='median')
 ffr.fits<-ffr.fits[fit.order]
 
 # fraction of replicates in which significant depletion occurred (in non-replacement datasets)
@@ -13,7 +13,7 @@ col.vec<-rep('black',length(ffr.fits))
 depleted <- unlist(lapply(ffr.fits, depletion.check, cutoff=0.7))
 col.vec[depleted>0] <- 'red'
 
-pdf(file="../../../../results/R/OnePredOnePrey_PhiDenom.pdf",height=6,width=5)
+pdf(file="../../../../results/R/OnePredOnePrey_SN1_PhiDenom.pdf",height=6,width=5)
 par(mar=c(3,10,1,1), mgp=c(1.5,0.1,0), tcl=-0.1, las=1, cex=0.7)
 plot.coefs(
   ffr.fits,
