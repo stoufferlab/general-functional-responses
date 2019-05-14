1b889293c54b8508e7e32863701c80edaf815530
marknovak <mnovak.m@gmail.com>
2019-05-14 16:27:05 -0700

    Merge AA plot scripts

--- code/R/One_Predator_One_Prey/plot_paramEsts/plot_AA.R
+++ code/R/One_Predator_One_Prey/plot_paramEsts/plot_AA.R
@@ -3,18 +3,20 @@ source('../../lib/plot_coefs.R')
 source('../../lib/depletion_check.R') 
 source('../../lib/holling_method_one_predator_one_prey.R')
 source('../../lib/ratio_method_one_predator_one_prey.R')
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 load('../../../../results/R/OnePredOnePrey_ffr.fits.Rdata')
-
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ffr.fits <- profile_coefs(ffr.fits, 
                           model='Arditi.Akcakaya',
-                          point.est='median')
+                          point.est='median',
+                          printWarnings = TRUE)
 
 save(ffr.fits, file='../../../../results/R/OnePredOnePrey_fits_profiled/ffr.fits.prof.AA.Rdata')
 # load('../../../../results/R/OnePredOnePrey_fits_profiled/ffr.fits.prof.AA.Rdata')
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# General data and plot preparations
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 fit.order <- order.of.fits(ffr.fits, order=TRUE, model="Arditi.Akcakaya", order.parm="Sample size")
 ffr.fits <- ffr.fits[fit.order]
 
@@ -51,7 +53,8 @@ par(mar=c(3,10,1,1), mgp=c(1.5,0.1,0), tcl=-0.1, las=1, cex=0.7)
      pch.vector=pch.vec, # delete or specify above plot()
      xlab="Arditi-Akcakaya interference rate (m)",
      labels=labels,
-     xlim=c(0,5)
+     xlim=c(0,5),
+     vertLines=c(0,1)
   )
   legend('right', 
          legend=c('Predator', 'Parasitoid', 'Replacement', 'Non-replacement', 'Profiled', 'Approximated','Bootstrapped'),
@@ -69,25 +72,24 @@ parm <- exp(parm)
 
 pdf('../../../../results/R/OnePredOnePrey_figs/OnePredOnePrey_AA_m_xy.pdf',height=3,width=4)
 par(cex=0.7,  mgp=c(1.5,0.1,0), tcl=-0.1)
-ylim <- c(0,5)
-plot(parm~sample.sizes,
-     ylim=ylim,
-     type='n',
-     log='x', 
-     xlab='Sample size (n)', 
-     ylab='Arditi-Akcakaya interference rate (m)')
-abline(h=c(0,1),lty=2,col='grey')
-arrows(sample.sizes[parm>ylim[2]], 1*ylim[2], 
-       sample.sizes[parm>ylim[2]], 1.03*ylim[2], 
-       length=0.02)
-text(sample.sizes[parm>ylim[2]],
-     0.98*ylim[2],
-     round(parm[parm>ylim[2]],0),
-     cex=0.5)
-points(sample.sizes, parm,
-       pch=21, bg='grey')
+  ylim <- c(0,5)
+  plot(parm~sample.sizes,
+       ylim=ylim,
+       type='n',
+       log='x', 
+       xlab='Sample size (n)', 
+       ylab='Arditi-Akcakaya interference rate (m)')
+  abline(h=c(0,1),lty=2,col='grey')
+  arrows(sample.sizes[parm>ylim[2]], 1*ylim[2], 
+         sample.sizes[parm>ylim[2]], 1.03*ylim[2], 
+         length=0.02)
+  text(sample.sizes[parm>ylim[2]],
+       0.98*ylim[2],
+       round(parm[parm>ylim[2]],0),
+       cex=0.5)
+  points(sample.sizes, parm,
+         pch=21, bg='grey')
 dev.off()
-
 
 pdf('../../../../results/R/OnePredOnePrey_figs/OnePredOnePrey_AA_m_hist.pdf',height=2.5,width=4)
 par(mar=c(3,3,1,1), mgp=c(1.5,0.2,0), tcl=-0.1, las=1, cex=0.7, yaxs='i',xaxs='i')
@@ -95,7 +97,7 @@ par(mar=c(3,3,1,1), mgp=c(1.5,0.2,0), tcl=-0.1, las=1, cex=0.7, yaxs='i',xaxs='i
        xlab='Arditi-Akcakaya interference rate (m)',
        main='',
        col='grey',
-       ylim=c(0,16),
+       ylim=c(0,12),
        xlim=c(0,5.5))
   abline(v=1,lty=3,lwd=1,col='black')
   box(lwd=1,bty='l')
@@ -114,10 +116,7 @@ plot.coefs(
   point.est='median',
   plot.SEs=TRUE,
   display.outlier.ests=TRUE,
-  color.factor='None', # 'None', 'Parasitoids' or 'Replacement'
-  color.vector=col.vec, # delete or specify above plot()
   xlab="Arditi-Akcakaya attack rate (a)",
-  ylab="",
   labels=labels,
   xlim=c(0,1)
 )
@@ -135,31 +134,28 @@ names(parm) <- paste0(names(parm), ' (',sample.sizes,')')
 
 pdf('../../../../results/R/OnePredOnePrey_figs/OnePredOnePrey_AA_a_xy.pdf',height=3,width=4)
 par(cex=0.7,  mgp=c(1.5,0.1,0), tcl=-0.1)
-ylim <- c(0,5)
-plot(parm~sample.sizes,
-     ylim=ylim,
-     type='n',
-     log='x', 
-     xlab='Sample size (n)', 
-     ylab='Arditi-Akcakaya attack rate (a)')
-abline(h=c(0,1),lty=2,col='grey')
-arrows(sample.sizes[parm>ylim[2]], 1*ylim[2], sample.sizes[parm>ylim[2]], 1.03*ylim[2], length=0.02)
-text(sample.sizes[parm>ylim[2]],0.98*ylim[2],round(parm[parm>ylim[2]],0),cex=0.5)
-points(sample.sizes,parm,
-       pch=21, bg='grey')
+  ylim <- c(0,5)
+  plot(parm~sample.sizes,
+       ylim=ylim,
+       type='n',
+       log='x', 
+       xlab='Sample size (n)', 
+       ylab='Arditi-Akcakaya attack rate (a)')
+  arrows(sample.sizes[parm>ylim[2]], 1*ylim[2], sample.sizes[parm>ylim[2]], 1.03*ylim[2], length=0.02)
+  text(sample.sizes[parm>ylim[2]],0.98*ylim[2],round(parm[parm>ylim[2]],0),cex=0.5)
+  points(sample.sizes,parm,
+         pch=21, bg='grey')
 dev.off()
 
 
 pdf('../../../../results/R/OnePredOnePrey_figs/OnePredOnePrey_AA_a_hist.pdf',height=2.5,width=4)
 par(mar=c(3,3,1,1), mgp=c(1.5,0.2,0), tcl=-0.1, las=1, cex=0.7, yaxs='i',xaxs='i')
-hist(parm[parm<5],breaks=50,
-     xlab='Arditi-Akcakaya attack rate (a)',
-     main='',
-     col='grey',
-     ylim=c(0,16),
-     xlim=c(0,5.5))
-abline(v=1,lty=3,lwd=1,col='black')
-box(lwd=1,bty='l')
+  hist(parm[parm<5],breaks=50,
+       xlab='Arditi-Akcakaya attack rate (a)',
+       main='',
+       col='grey',
+       ylim=c(0,60),
+       xlim=c(0,5.5))
+  box(lwd=1,bty='l')
 dev.off()
-
 
