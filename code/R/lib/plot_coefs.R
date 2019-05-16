4289fb2da96426b78f02d11abda769f8bbe32190
marknovak <mnovak.m@gmail.com>
2019-05-16 13:52:34 -0700

    Use readRDS in bundle_fits()

--- code/R/lib/plot_coefs.R
+++ code/R/lib/plot_coefs.R
@@ -6,7 +6,9 @@ bundle_fits <- function(dir){
                      include.dirs=FALSE)
   bundled_fits <- list()
   for (i in 1:length(fits)){
-    load(fits[i]) # returns the 'ffr.fit' object
+    print(fits[i])
+    ffr.fit <- readRDS(fits[i])
+    print(ffr.fit$study.info$datasetName)
     bundled_fits[[i]] <- ffr.fit
   }
   return(bundled_fits)
