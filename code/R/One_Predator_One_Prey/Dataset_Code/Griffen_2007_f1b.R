9c95e22b081cfa8bfc0a9a5690f3919a56588956
Mark Novak <mnovak.m@gmail.com>
2019-05-16 23:24:49 -0700

    Rename Griffen datasets

---
+++ code/R/One_Predator_One_Prey/Dataset_Code/Griffen_2007_f1b.R
@@ -0,0 +1,9 @@
+
+# read in the simplify the raw data
+datadir <- 'Griffen_2007'
+filename <- 'Griffen_2007_Fig1b.csv'
+rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
+
+# turn into a standard dataframe with standard column names
+d <- rawdata[,c('Pred','Prey','TotalEaten.Mean','TotalEaten.SE','n','Time')]
+colnames(d) <- c("Npredator", "Nprey", "Nconsumed.mean", "Nconsumed.se", "n",'Time')
