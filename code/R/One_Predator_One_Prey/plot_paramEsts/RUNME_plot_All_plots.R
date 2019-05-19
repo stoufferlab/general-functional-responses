b9b5340c057fde76442bed1ac17b991d5e6b344e
Mark Novak <mnovak.m@gmail.com>
2019-05-18 21:30:03 -0700

    Add RUNME script to run all plotting scripts

---
+++ code/R/One_Predator_One_Prey/plot_paramEsts/RUNME_plot_All_plots.R
@@ -0,0 +1,14 @@
+
+
+source('plot_AICc.R')
+source('plot_RMSE.R')
+
+source('plot_AA.R')
+source('plot_AA2.R')
+source('plot_AA_m_DeLong.R')
+
+source('plot_BD.R')
+source('plot_H2.R')
+source('plot_HV.R')
+
+source('plot_SN1.R')
