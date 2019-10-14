89497994b9c4c12397895504e55a8f518b0af015
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2019-10-14 14:19:45 +1300

    removing one dataset; the folder for another had its name changed

--- code/R/One_Predator_Two_Prey/Dataset_Code/Barnhisel_2004.R
+++
@@ -1,9 +0,0 @@
-
-# read in the simplify the raw data
-datadir <- 'Barnhisel_2004'
-filename <- 'Barnhisel_2004.csv'
-rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
-
-# rename to standard column names used in fitting code
-d <- rawdata[,c('Preds','Prey1.Daphnia','Prey1.Eaten','Prey1.Eaten.SE','Prey2.Bythotrephes','Prey2.Eaten','Prey2.Eaten.se','n')]
-colnames(d) <- c("Npredator", "Nprey1", "Nconsumed1.mean", "Nconsumed1.se", "Nprey2", "Nconsumed2.mean", "Nconsumed2.se", "n")
--- code/R/One_Predator_Two_Prey/Dataset_Code/Krylov_1992ii.R
+++ code/R/One_Predator_Two_Prey/Dataset_Code/Krylov_1992ii.R
@@ -1,9 +1,9 @@
 
 # read in the simplify the raw data
-datadir <- 'Krylov_1992ii'
+datadir <- 'Krylov_1992'
 filename <- 'Krylov_1992ii.csv'
 rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
 
 # rename to standard column names used in fitting code
-d <- rawdata[,c('Preds','Prey1.S','TotalPrey1Eaten.Mean','TotalPrey1Eaten.SE','Prey2.L','TotalPrey2Eaten.Mean','TotalPrey2Eaten.SE','n')]
-colnames(d) <- c("Npredator", "Nprey1", "Nconsumed1.mean", "Nconsumed1.se", "Nprey2", "Nconsumed2.mean", "Nconsumed2.se", "n")
+d <- rawdata[,c('Preds','Prey1.S','TotalPrey1Eaten.Mean','TotalPrey1Eaten.SE','Prey2.L','TotalPrey2Eaten.Mean','TotalPrey2Eaten.SE','n','Time')]
+colnames(d) <- c("Npredator", "Nprey1", "Nconsumed1.mean", "Nconsumed1.se", "Nprey2", "Nconsumed2.mean", "Nconsumed2.se", "n", "Time")
