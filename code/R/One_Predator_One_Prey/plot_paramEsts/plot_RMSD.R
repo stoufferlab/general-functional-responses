5c6d31390589089987ddfe116254c9641eb208ac
Mark Novak <mnovak.m@gmail.com>
2020-02-21 13:43:58 -0800

    Apply new AICc figure format to RMSD figure

--- code/R/One_Predator_One_Prey/plot_paramEsts/plot_RMSD.R
+++ code/R/One_Predator_One_Prey/plot_paramEsts/plot_RMSD.R
@@ -37,46 +37,49 @@ RMSDs <- data.frame(RMSD.H1, RMSD.H2, RMSD.BD, RMSD.CM, RMSD.R, RMSD.AG, RMSD.HV
 colnames(RMSDs) <- sub('RMSD.', '', colnames(RMSDs))
 
 # Define color of each model
-parm.k<-c(1,2,3,3, 1,2,2,3)
-# CR<-brewer.pal(n = 8, name = 'RdBu')
-# Mcols <- c(CR[5:8],CR[4:1])
-# Mpch <- c(rep(21,4),rep(22,4))
-CR<-brewer.pal(3,'YlOrRd')
-Mcols <- c(CR[parm.k[1:4]],CR[parm.k[5:8]])
+colnames(RMSDs)
+parm.k.1<-c(1,2,3,3)
+parm.k.2<-c(1,2,2,3)
+CR1<-brewer.pal(n = 3, name = 'YlOrRd')
+CR2<-brewer.pal(n = 3, name = 'Blues')
+Mcols<-c(CR1[parm.k.1],CR2[parm.k.2])
+
 Mpch <- c(rep(21,4),rep(22,4))
-Mpch2 <- c(NA,NA,NA,3,NA,NA,4,NA) # to overlay x's to differentiate models
-  
+Mpch2 <- c(NA,NA,NA,21,NA,NA,22,NA) # overlay symbols to differentiate equal k models
+
 minRMSDs <- apply(RMSDs, 1, min)
-dRMSDs <- RMSDs - minRMSDs
-rnkRMSDs <- t(apply(dRMSDs, 1, rank, ties.method='first'))
-colnames(rnkRMSDs) <- colnames(RMSDs)
+delV <- RMSDs - minRMSDs
+rnks <- t(apply(delV, 1, rank, ties.method='first'))
+colnames(rnks) <- colnames(RMSDs)
 
 # Define delta RMSD cut-off for "well performing" models:
 # Which models have an RMSD that is less than 1% of the data average (repeat for raw and bootstrapped datasets, which will each throw errors where the other does not apply, and merge)
-delRMSDcutoff <- unlist(lapply(ffr.fits, 
+delcutoff <- unlist(lapply(ffr.fits, 
                                function(x){0.01*mean(x$study.info$data.Nconsumed)}))
-delRMSDcutoff2 <- unlist(lapply(ffr.fits, 
+delcutoff2 <- unlist(lapply(ffr.fits, 
                                 function(x){0.01*mean(x$study.info$data.Nconsumed.mean)}))
-delRMSDcutoff[which(is.na(delRMSDcutoff))] <- delRMSDcutoff2[which(!is.na(delRMSDcutoff2))]
+delcutoff[which(is.na(delcutoff))] <- delcutoff2[which(!is.na(delcutoff2))]
 
 #~~~~~~~~~~~
 # Rank order
 #~~~~~~~~~~~
-pdf('../../../../results/R/OnePredOnePrey_figs/OnePredOnePrey_RMSD_ranks.pdf',height=6,width=2.25)
-  par(mar=c(3,7,2.5,0.5), mgp=c(1.5,0.2,0), tcl=-0.1, las=1, cex=0.7, yaxs='i')
-    plot(1:nrow(rnkRMSDs), 1:nrow(rnkRMSDs),
+pdf('../../../../results/R/OnePredOnePrey_figs/OnePredOnePrey_RMSD_ranks.pdf',
+    height=6.5,width=2)
+par(mar=c(2,6.5,4,0.5), mgp=c(1.5,0.2,0), tcl=-0.1, las=1, cex=0.7, yaxs='i')
+    plot(1:nrow(rnks), 1:nrow(rnks),
          type='n', yaxt='n',
-         xlim=c(1,ncol(rnkRMSDs)),
-         ylim=c(0,nrow(rnkRMSDs)+1),
-         xlab='Model rank by RMSD',
+         xlim=c(1,ncol(rnks)),
+         ylim=c(0,nrow(rnks)+1),
+         xlab='',
          ylab='',
          axes=F)
+    title(xlab='Model rank by RMSD',line=1)
     rect(par("usr")[1],par("usr")[3],par("usr")[2],par("usr")[4], col = "white")
-    axis(2, at=1:nrow(rnkRMSDs), labels=labels, cex.axis=0.5, las=2)
+    axis(2, at=1:nrow(rnks), labels=labels, cex.axis=0.5, las=2)
     axis(1, cex.axis=0.7, mgp=c(1.25,0,0))
 
     # Which models have "reasonable" delta-RMSD?
-    xats <-table(which(dRMSDs < delRMSDcutoff, arr.ind=T)[,1])+0.5
+    xats <-table(which(delV < delcutoff, arr.ind=T)[,1])+0.5
     yats <- 0:(length(xats))+0.5
     segments(xats,yats[-length(yats)],xats,yats[-1],col='black')
     segments(xats[-length(xats)],yats+1,xats[-1],yats+1,col='black')
@@ -85,32 +88,63 @@ pdf('../../../../results/R/OnePredOnePrey_figs/OnePredOnePrey_RMSD_ranks.pdf',he
     pyats<-rep(0:(length(xats)),each=2)+0.5
     polygon(pxats,pyats,col='grey90',border=NA)
     
-    for(m in 1:ncol(rnkRMSDs)){
-      points(rnkRMSDs[,m], 1:nrow(rnkRMSDs), 
-             type='p', col='black', 
+    for(m in 1:ncol(rnks)){
+      points(rnks[,m], 1:nrow(rnks), 
+             type='p',  col='black', 
              bg=Mcols[m], pch=Mpch[m],
              cex=1, lwd=0.2)
-      # Overlay x's
-      points(rnkRMSDs[,m], 1:nrow(rnkRMSDs), 
+      # Overlay secondary symbos
+      points(rnks[,m], 1:nrow(rnks), 
              type='p',  col='black', 
-             pch=Mpch2[m],
-             cex=1, lwd=0.2)
-    }
+             bg='white',pch=Mpch2[m],
+             cex=0.3, lwd=0.2)
+    } 
     box(lwd=1)
-  par(xpd=TRUE)
-    legend(-4,nrow(rnkRMSDs)+6,legend=colnames(rnkRMSDs),
-           pch=Mpch,pt.bg=Mcols, col='black', bg='white',
-           horiz=TRUE, pt.cex=1.1,cex=0.6, ncol=2, title='Model')
-    # overlay x's
-    legend(-4,nrow(rnkRMSDs)+6,legend=colnames(rnkRMSDs),
-           pch=Mpch2, pt.bg=NA, col='black', bg=NA,
-           horiz=TRUE, pt.cex=1.1,cex=0.6, ncol=2, title='')
+    
+    par(xpd=TRUE)
+    vadj <- 7.5
+    xadj <- 1.5
+    mods<-c(1,5)
+    legend(xadj,nrow(rnks)+vadj,legend=colnames(rnks)[mods],
+           pch=Mpch[mods], pt.bg=Mcols[mods], col='black', bg='white',
+           pt.cex=1.1,cex=0.6, ncol=1, pt.lwd=0.8, 
+           title=expression(italic(k)==1), bty='n')
+    # overlay secondary symbols
+    legend(xadj,nrow(rnks)+vadj,legend=colnames(rnks)[mods],
+           pch=Mpch2[mods], pt.bg='white', col='black', bg=NA,
+           pt.cex=0.3,cex=0.6, ncol=1, pt.lwd=0.3,
+           title=expression(italic(k)==1), bty='n')
+    
+    mods<-c(2,6,7)
+    legend(xadj+2,nrow(rnks)+vadj,legend=colnames(rnks)[mods],
+           pch=Mpch[mods], pt.bg=Mcols[mods], col='black', bg='white',
+           pt.cex=1.1,cex=0.6, ncol=1, pt.lwd=0.8,
+           title=expression(italic(k)==2), bty='n')
+    # overlay secondary symbols
+    legend(xadj+2,nrow(rnks)+vadj,legend=colnames(rnks)[mods],
+           pch=Mpch2[mods], pt.bg='white', col='black', bg=NA,
+           pt.cex=0.3,cex=0.6, ncol=1, pt.lwd=0.3,
+           title=expression(italic(k)==2), bty='n')
+    
+    mods<-c(3,4,8)
+    legend(xadj+4,nrow(rnks)+vadj,legend=colnames(rnks)[mods],
+           pch=Mpch[mods], pt.bg=Mcols[mods], col='black', bg='white',
+           pt.cex=1.1,cex=0.6, ncol=1, pt.lwd=0.8,
+           title=expression(italic(k)==3), bty='n')
+    # overlay secondary symbols
+    legend(xadj+4,nrow(rnks)+vadj,legend=colnames(rnks)[mods],
+           pch=Mpch2[mods], pt.bg='white', col='black', bg=NA,
+           pt.cex=0.3,cex=0.6, ncol=1, pt.lwd=0.3,
+           title=expression(italic(k)==3), bty='n')
+    
+    rect(1.5,nrow(rnks)+vadj+1.75,7.5,nrow(rnks)+vadj-6)
+    text(xadj+3,nrow(rnks)+vadj+0.75,'Models', adj=0.5, cex=0.8)
 dev.off()
 
 
 # ~~~~~~~~~
 # Count times each model is in each rank
-Cnt<-apply(rnkRMSDs,2,function(x){table(factor(x,levels=1:ncol(rnkRMSDs)))})
+Cnt<-apply(rnks,2,function(x){table(factor(x,levels=1:ncol(rnks)))})
 Cnt
 pCnt <- round(prop.table(Cnt,2)*100,1)
 pCnt
@@ -135,14 +169,14 @@ setwd(wd)
 # ~~~~~~~~~
 
 # Of the times that AA is ranked first, how often are BD and CM  within X RMSD units?
-cnt<-apply(dRMSDs[rnkRMSDs[,ncol(rnkRMSDs)]==1,3:4] < delRMSDcutoff, 2, sum)
+cnt<-apply(delV[rnks[,ncol(rnks)]==1,3:4] < delcutoff, 2, sum)
 cnt
 Cnt[1,'AA']
 cnt/Cnt[1,'AA']
 # Concl: BD and CM are within cutoff 60% of the time.
 
 # Of times that AA is ranked first, how often are *either* BD and CM within X RMSD units?
-cnt<-sum(apply(dRMSDs[rnkRMSDs[,ncol(rnkRMSDs)]==1,3:4] < delRMSDcutoff, 1, sum)>0)
+cnt<-sum(apply(delV[rnks[,ncol(rnks)]==1,3:4] < delcutoff, 1, sum)>0)
 cnt
 cnt/Cnt[1,'AA']
 # Concl: BD or CM are within cutoff ~76% of the time
@@ -150,7 +184,7 @@ cnt/Cnt[1,'AA']
 # ~~~~~~~~~
 # What about for datasets that have a sample size of at least X?
 SScut <- 50
-Cnt<-apply(rnkRMSDs[sample.sizes>=SScut,],2,function(x){table(factor(x,levels=1:ncol(rnkRMSDs)))})
+Cnt<-apply(rnks[sample.sizes>=SScut,],2,function(x){table(factor(x,levels=1:ncol(rnks)))})
 Cnt
 pCnt <- round(prop.table(Cnt,2)*100,1)
 pCnt
