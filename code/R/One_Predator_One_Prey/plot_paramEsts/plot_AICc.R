45fd1d85ec850feb9c5a90a4a2cf092e50e3644a
Mark Novak <mnovak.m@gmail.com>
2020-03-02 15:53:03 -0800

    Add rank (AICc and RMSD) by sample size plots

--- code/R/One_Predator_One_Prey/plot_paramEsts/plot_AICc.R
+++ code/R/One_Predator_One_Prey/plot_paramEsts/plot_AICc.R
@@ -45,12 +45,12 @@ Mpch <- c(rep(21,4),rep(22,4))
 Mpch2 <- c(NA,NA,NA,21,NA,NA,22,NA) # overlay symbols to differentiate equal k models
 
 minAICcs <- apply(AICcs, 1, min)
-delV <- AICcs - minAICcs
-rnks <- t(apply(delV, 1, rank, ties.method='first'))
-colnames(rnks) <- colnames(AICcs)
+delV.AICc <- AICcs - minAICcs
+rnks.AICc <- t(apply(delV.AICc, 1, rank, ties.method='first'))
+colnames(rnks.AICc) <- colnames(AICcs)
 
 # Define delta AICc cut-off for "indistinguishably well performing" models
-delcutoff <- 2
+cut.AIC <- 2
 
 #~~~~~~~~~~~
 # Rank order
@@ -58,20 +58,20 @@ delcutoff <- 2
 pdf('../../../../results/R/OnePredOnePrey_figs/OnePredOnePrey_AICc_ranks.pdf',
     height=6.5,width=2)
   par(mar=c(2,6.5,4,0.5), mgp=c(1.5,0.2,0), tcl=-0.1, las=1, cex=0.7, yaxs='i')
-    plot(1:nrow(rnks), 1:nrow(rnks),
+    plot(1:nrow(rnks.AICc), 1:nrow(rnks.AICc),
          type='n', yaxt='n',
-         xlim=c(1,ncol(rnks)),
-         ylim=c(0,nrow(rnks)+1),
+         xlim=c(1,ncol(rnks.AICc)),
+         ylim=c(0,nrow(rnks.AICc)+1),
          xlab='',
          ylab='',
          axes=F)
     title(xlab='Model rank by AICc',line=1)
     rect(par("usr")[1],par("usr")[3],par("usr")[2],par("usr")[4], col = "white") # grey30
-    axis(2, at=1:nrow(rnks), labels=labels, cex.axis=0.5, las=2)
+    axis(2, at=1:nrow(rnks.AICc), labels=labels, cex.axis=0.5, las=2)
     axis(1, cex.axis=0.7, mgp=c(1.25,0,0))
 
     # Which models have delta-AICc within X=2 of best-performing model?
-    xats <-table(which(delV < delcutoff, arr.ind=T)[,1])+0.5
+    xats <-table(which(delV.AICc < cut.AIC, arr.ind=T)[,1])+0.5
     yats <- 0:(length(xats))+0.5
     segments(xats,yats[-length(yats)],xats,yats[-1],col='black')
     segments(xats[-length(xats)],yats+1,xats[-1],yats+1,col='black')
@@ -80,13 +80,13 @@ pdf('../../../../results/R/OnePredOnePrey_figs/OnePredOnePrey_AICc_ranks.pdf',
     pyats<-rep(0:(length(xats)),each=2)+0.5
     polygon(pxats,pyats,col='grey90',border=NA)
     
-    for(m in 1:ncol(rnks)){
-      points(rnks[,m], 1:nrow(rnks), 
+    for(m in 1:ncol(rnks.AICc)){
+      points(rnks.AICc[,m], 1:nrow(rnks.AICc), 
              type='p',  col='black', 
              bg=Mcols[m], pch=Mpch[m],
              cex=1, lwd=0.2)
       # Overlay secondary symbos
-      points(rnks[,m], 1:nrow(rnks), 
+      points(rnks.AICc[,m], 1:nrow(rnks.AICc), 
              type='p',  col='black', 
              bg='white',pch=Mpch2[m],
              cex=0.3, lwd=0.2)
@@ -97,45 +97,45 @@ pdf('../../../../results/R/OnePredOnePrey_figs/OnePredOnePrey_AICc_ranks.pdf',
   vadj <- 7.5
   xadj <- 1.5
   mods<-c(1,5)
-    legend(xadj,nrow(rnks)+vadj,legend=colnames(rnks)[mods],
+    legend(xadj,nrow(rnks.AICc)+vadj,legend=colnames(rnks.AICc)[mods],
            pch=Mpch[mods], pt.bg=Mcols[mods], col='black', bg='white',
             pt.cex=1.1,cex=0.6, ncol=1, pt.lwd=0.8, 
            title=expression(italic(k)==1), bty='n')
     # overlay secondary symbols
-    legend(xadj,nrow(rnks)+vadj,legend=colnames(rnks)[mods],
+    legend(xadj,nrow(rnks.AICc)+vadj,legend=colnames(rnks.AICc)[mods],
            pch=Mpch2[mods], pt.bg='white', col='black', bg=NA,
            pt.cex=0.3,cex=0.6, ncol=1, pt.lwd=0.3,
            title=expression(italic(k)==1), bty='n')
     
     mods<-c(2,6,7)
-    legend(xadj+2,nrow(rnks)+vadj,legend=colnames(rnks)[mods],
+    legend(xadj+2,nrow(rnks.AICc)+vadj,legend=colnames(rnks.AICc)[mods],
            pch=Mpch[mods], pt.bg=Mcols[mods], col='black', bg='white',
            pt.cex=1.1,cex=0.6, ncol=1, pt.lwd=0.8,
            title=expression(italic(k)==2), bty='n')
     # overlay secondary symbols
-    legend(xadj+2,nrow(rnks)+vadj,legend=colnames(rnks)[mods],
+    legend(xadj+2,nrow(rnks.AICc)+vadj,legend=colnames(rnks.AICc)[mods],
            pch=Mpch2[mods], pt.bg='white', col='black', bg=NA,
            pt.cex=0.3,cex=0.6, ncol=1, pt.lwd=0.3,
            title=expression(italic(k)==2), bty='n')
     
     mods<-c(3,4,8)
-    legend(xadj+4,nrow(rnks)+vadj,legend=colnames(rnks)[mods],
+    legend(xadj+4,nrow(rnks.AICc)+vadj,legend=colnames(rnks.AICc)[mods],
            pch=Mpch[mods], pt.bg=Mcols[mods], col='black', bg='white',
            pt.cex=1.1,cex=0.6, ncol=1, pt.lwd=0.8,
            title=expression(italic(k)==3), bty='n')
     # overlay secondary symbols
-    legend(xadj+4,nrow(rnks)+vadj,legend=colnames(rnks)[mods],
+    legend(xadj+4,nrow(rnks.AICc)+vadj,legend=colnames(rnks.AICc)[mods],
            pch=Mpch2[mods], pt.bg='white', col='black', bg=NA,
            pt.cex=0.3,cex=0.6, ncol=1, pt.lwd=0.3,
            title=expression(italic(k)==3), bty='n')
     
-    rect(1.5,nrow(rnks)+vadj+1.75,7.5,nrow(rnks)+vadj-6)
-    text(xadj+3,nrow(rnks)+vadj+0.75,'Models', adj=0.5, cex=0.8)
+    rect(1.5,nrow(rnks.AICc)+vadj+1.75,7.5,nrow(rnks.AICc)+vadj-6)
+    text(xadj+3,nrow(rnks.AICc)+vadj+0.75,'Models', adj=0.5, cex=0.8)
 dev.off()
 
 # ~~~~~~~~~
 # Count times each model is in each rank
-Cnt<-apply(rnks,2,function(x){table(factor(x,levels=1:ncol(rnks)))})
+Cnt<-apply(rnks.AICc,2,function(x){table(factor(x,levels=1:ncol(rnks.AICc)))})
 Cnt
 pCnt <- round(prop.table(Cnt,2)*100,1)
 pCnt
@@ -159,25 +159,90 @@ latex(tab_Cnt,file='OnePredOnePrey_AICc_rankings.tex',label='table:AICc_rankings
 setwd(wd)
 # ~~~~~~~~~
 
-# Of the times that AA is ranked first, how often are BD and CM  within X AICc units?
-cnt<-apply(delV[rnks[,ncol(rnks)]==1,3:4] < delcutoff, 2, sum)
+# Of the times that AA is ranked first, how often are BD and CM within X AICc units?
+cnt<-apply(delV.AICc[rnks.AICc[,ncol(rnks.AICc)]==1,3:4] < cut.AIC, 2, sum)
 cnt
 Cnt[1,'AA']
 cnt/Cnt[1,'AA']
-# Concl: BD is within 2 dAICc units > 1/2 the time, and CM is > 1/4 of the time
+# Concl: BD is within 2 dAICc units > 1/3 the time, and CM is > 1/4 of the time
 
 # Of times that AA is ranked first, how often are *either* BD and CM within X AICc units?
-cnt<-sum(apply(delV[rnks[,ncol(rnks)]==1,3:4] < delcutoff, 1, sum)>0)
+cnt<-sum(apply(delV.AICc[rnks.AICc[,ncol(rnks.AICc)]==1,3:4] < cut.AIC, 1, sum)>0)
 cnt
 cnt/Cnt[1,'AA']
 # Concl: BD or CM are within 2 dAICc units ~60% of the time
 
+# How many times is a single model the only best model?
+cnt<-sum(apply(delV.AICc < cut.AIC, 1, sum)==1)
+cnt/nrow(delV.AICc)
+
 # ~~~~~~~~~
 # What about for datasets that have a sample size of at least X?
+SScuts <- seq(5,300,by=1)
+fFirst.AICc<-fSecnd.AICc<-dim(0)
+for(SScut in SScuts){
+  Cnt<-apply(rnks.AICc[sample.sizes>=SScut,],2,function(x){table(factor(x,levels=1:ncol(rnks.AICc)))})
+  pCnt <- prop.table(Cnt,2)
+  fFirst.AICc <- rbind(fFirst.AICc,pCnt[1,])
+  fSecnd.AICc <- rbind(fSecnd.AICc,pCnt[2,])
+}
+
+# Darken colors of linear models
+Mcols[c(1,5)]<-'grey40'
+
+ltys <- c(1,1,1,6,2,2,3,1)
+pdf('../../../../results/R/OnePredOnePrey_figs/OnePredOnePrey_AICc_rankBySS.pdf',
+    height=4,width=3)
+par(mar=c(2,6.5,4,0.5), mgp=c(1.5,0.2,0), tcl=-0.1, las=1, cex=0.8)
+  layout(matrix(c(1,2,3),ncol=1), heights=c(1,3,3)) 
+  par(mar=c(0,3,0,0))
+  plot(1,1,type="n", axes=F, xlab="", ylab="")
+  
+  xadj<-0.7
+  yadj<-1.2
+  lcex<-0.7
+  
+  mods<-c(1,5)
+  legend(xadj,yadj,legend=colnames(fFirst.AICc)[mods],
+         lty=ltys[mods], col=Mcols[mods],lwd=1.5,
+         cex=lcex, ncol=1, pt.lwd=0.8, 
+         title=expression(italic(k)==1), bty='n')
+  
+  mods<-c(2,6,7)
+  legend(xadj+0.2,yadj,legend=colnames(fFirst.AICc)[mods],
+         lty=ltys[mods], col=Mcols[mods],lwd=1.5,
+         pt.cex=1.1,cex=lcex, ncol=1, pt.lwd=0.8,
+         title=expression(italic(k)==2), bty='n')
+  
+  mods<-c(3,4,8)
+  legend(xadj+0.4,yadj,legend=colnames(fFirst.AICc)[mods],
+         lty=ltys[mods], col=Mcols[mods],lwd=1.5,
+         pt.cex=1.1,cex=lcex, ncol=1, pt.lwd=0.8,
+         title=expression(italic(k)==3), bty='n')
+  
+  text(xadj+0.3, yadj+0.1, 'Models', adj=0.5, cex=1)
+  
+  
+  par(mar=c(3,3,0.5,1))
+  matplot(SScuts,fFirst.AICc,las=1,type='l', col=Mcols,lty=ltys,cex=0.5,
+          ylim=c(0,0.6),lwd=1.5,
+          ylab='Fraction in first rank',
+          xlab='Sample size greater than...')
+  legend('topleft',legend='A',inset=-0.05,bty='n',cex=1.2)
+  matplot(SScuts,fSecnd.AICc,las=1,type='l', col=Mcols,lty=ltys,cex=0.5,
+          ylim=c(0,0.8),lwd=1.5,
+          ylab='Fraction in second rank',
+          xlab='Sample size greater than...')
+  legend('topleft',legend='B',inset=-0.05,bty='n',cex=1.2)
+dev.off()
+
+
+
 SScut <- 50
-Cnt<-apply(rnks[sample.sizes>=SScut,],2,function(x){table(factor(x,levels=1:ncol(rnks)))})
+Cnt<-apply(rnks.AICc[sample.sizes>=SScut,],2,function(x){table(factor(x,levels=1:ncol(rnks.AICc)))})
 Cnt
-pCnt <- round(prop.table(Cnt,2)*100,1)
+sum(Cnt[,1])
+pCnt <- prop.table(Cnt,2)
 pCnt
 
 # Either pass counts or combine counts and proportions before exporting table
--- code/R/One_Predator_One_Prey/plot_paramEsts/plot_RMSD.R
+++ code/R/One_Predator_One_Prey/plot_paramEsts/plot_RMSD.R
@@ -48,17 +48,17 @@ Mpch <- c(rep(21,4),rep(22,4))
 Mpch2 <- c(NA,NA,NA,21,NA,NA,22,NA) # overlay symbols to differentiate equal k models
 
 minRMSDs <- apply(RMSDs, 1, min)
-delV <- RMSDs - minRMSDs
-rnks <- t(apply(delV, 1, rank, ties.method='first'))
-colnames(rnks) <- colnames(RMSDs)
+delV.RMSD <- RMSDs - minRMSDs
+rnks.RMSD <- t(apply(delV.RMSD, 1, rank, ties.method='first'))
+colnames(rnks.RMSD) <- colnames(RMSDs)
 
 # Define delta RMSD cut-off for "well performing" models:
 # Which models have an RMSD that is less than 1% of the data average (repeat for raw and bootstrapped datasets, which will each throw errors where the other does not apply, and merge)
-delcutoff <- unlist(lapply(ffr.fits, 
+cut.RMSD <- unlist(lapply(ffr.fits, 
                                function(x){0.01*mean(x$study.info$data.Nconsumed)}))
-delcutoff2 <- unlist(lapply(ffr.fits, 
+cut.RMSD2 <- unlist(lapply(ffr.fits, 
                                 function(x){0.01*mean(x$study.info$data.Nconsumed.mean)}))
-delcutoff[which(is.na(delcutoff))] <- delcutoff2[which(!is.na(delcutoff2))]
+cut.RMSD[which(is.na(cut.RMSD))] <- cut.RMSD2[which(!is.na(cut.RMSD2))]
 
 #~~~~~~~~~~~
 # Rank order
@@ -66,20 +66,20 @@ delcutoff[which(is.na(delcutoff))] <- delcutoff2[which(!is.na(delcutoff2))]
 pdf('../../../../results/R/OnePredOnePrey_figs/OnePredOnePrey_RMSD_ranks.pdf',
     height=6.5,width=2)
 par(mar=c(2,6.5,4,0.5), mgp=c(1.5,0.2,0), tcl=-0.1, las=1, cex=0.7, yaxs='i')
-    plot(1:nrow(rnks), 1:nrow(rnks),
+    plot(1:nrow(rnks.RMSD), 1:nrow(rnks.RMSD),
          type='n', yaxt='n',
-         xlim=c(1,ncol(rnks)),
-         ylim=c(0,nrow(rnks)+1),
+         xlim=c(1,ncol(rnks.RMSD)),
+         ylim=c(0,nrow(rnks.RMSD)+1),
          xlab='',
          ylab='',
          axes=F)
     title(xlab='Model rank by RMSD',line=1)
     rect(par("usr")[1],par("usr")[3],par("usr")[2],par("usr")[4], col = "white")
-    axis(2, at=1:nrow(rnks), labels=labels, cex.axis=0.5, las=2)
+    axis(2, at=1:nrow(rnks.RMSD), labels=labels, cex.axis=0.5, las=2)
     axis(1, cex.axis=0.7, mgp=c(1.25,0,0))
 
     # Which models have "reasonable" delta-RMSD?
-    xats <-table(which(delV < delcutoff, arr.ind=T)[,1])+0.5
+    xats <-table(which(delV.RMSD < cut.RMSD, arr.ind=T)[,1])+0.5
     yats <- 0:(length(xats))+0.5
     segments(xats,yats[-length(yats)],xats,yats[-1],col='black')
     segments(xats[-length(xats)],yats+1,xats[-1],yats+1,col='black')
@@ -88,13 +88,13 @@ par(mar=c(2,6.5,4,0.5), mgp=c(1.5,0.2,0), tcl=-0.1, las=1, cex=0.7, yaxs='i')
     pyats<-rep(0:(length(xats)),each=2)+0.5
     polygon(pxats,pyats,col='grey90',border=NA)
     
-    for(m in 1:ncol(rnks)){
-      points(rnks[,m], 1:nrow(rnks), 
+    for(m in 1:ncol(rnks.RMSD)){
+      points(rnks.RMSD[,m], 1:nrow(rnks.RMSD), 
              type='p',  col='black', 
              bg=Mcols[m], pch=Mpch[m],
              cex=1, lwd=0.2)
       # Overlay secondary symbos
-      points(rnks[,m], 1:nrow(rnks), 
+      points(rnks.RMSD[,m], 1:nrow(rnks.RMSD), 
              type='p',  col='black', 
              bg='white',pch=Mpch2[m],
              cex=0.3, lwd=0.2)
@@ -105,46 +105,46 @@ par(mar=c(2,6.5,4,0.5), mgp=c(1.5,0.2,0), tcl=-0.1, las=1, cex=0.7, yaxs='i')
     vadj <- 7.5
     xadj <- 1.5
     mods<-c(1,5)
-    legend(xadj,nrow(rnks)+vadj,legend=colnames(rnks)[mods],
+    legend(xadj,nrow(rnks.RMSD)+vadj,legend=colnames(rnks.RMSD)[mods],
            pch=Mpch[mods], pt.bg=Mcols[mods], col='black', bg='white',
            pt.cex=1.1,cex=0.6, ncol=1, pt.lwd=0.8, 
            title=expression(italic(k)==1), bty='n')
     # overlay secondary symbols
-    legend(xadj,nrow(rnks)+vadj,legend=colnames(rnks)[mods],
+    legend(xadj,nrow(rnks.RMSD)+vadj,legend=colnames(rnks.RMSD)[mods],
            pch=Mpch2[mods], pt.bg='white', col='black', bg=NA,
            pt.cex=0.3,cex=0.6, ncol=1, pt.lwd=0.3,
            title=expression(italic(k)==1), bty='n')
     
     mods<-c(2,6,7)
-    legend(xadj+2,nrow(rnks)+vadj,legend=colnames(rnks)[mods],
+    legend(xadj+2,nrow(rnks.RMSD)+vadj,legend=colnames(rnks.RMSD)[mods],
            pch=Mpch[mods], pt.bg=Mcols[mods], col='black', bg='white',
            pt.cex=1.1,cex=0.6, ncol=1, pt.lwd=0.8,
            title=expression(italic(k)==2), bty='n')
     # overlay secondary symbols
-    legend(xadj+2,nrow(rnks)+vadj,legend=colnames(rnks)[mods],
+    legend(xadj+2,nrow(rnks.RMSD)+vadj,legend=colnames(rnks.RMSD)[mods],
            pch=Mpch2[mods], pt.bg='white', col='black', bg=NA,
            pt.cex=0.3,cex=0.6, ncol=1, pt.lwd=0.3,
            title=expression(italic(k)==2), bty='n')
     
     mods<-c(3,4,8)
-    legend(xadj+4,nrow(rnks)+vadj,legend=colnames(rnks)[mods],
+    legend(xadj+4,nrow(rnks.RMSD)+vadj,legend=colnames(rnks.RMSD)[mods],
            pch=Mpch[mods], pt.bg=Mcols[mods], col='black', bg='white',
            pt.cex=1.1,cex=0.6, ncol=1, pt.lwd=0.8,
            title=expression(italic(k)==3), bty='n')
     # overlay secondary symbols
-    legend(xadj+4,nrow(rnks)+vadj,legend=colnames(rnks)[mods],
+    legend(xadj+4,nrow(rnks.RMSD)+vadj,legend=colnames(rnks.RMSD)[mods],
            pch=Mpch2[mods], pt.bg='white', col='black', bg=NA,
            pt.cex=0.3,cex=0.6, ncol=1, pt.lwd=0.3,
            title=expression(italic(k)==3), bty='n')
     
-    rect(1.5,nrow(rnks)+vadj+1.75,7.5,nrow(rnks)+vadj-6)
-    text(xadj+3,nrow(rnks)+vadj+0.75,'Models', adj=0.5, cex=0.8)
+    rect(1.5,nrow(rnks.RMSD)+vadj+1.75,7.5,nrow(rnks.RMSD)+vadj-6)
+    text(xadj+3,nrow(rnks.RMSD)+vadj+0.75,'Models', adj=0.5, cex=0.8)
 dev.off()
 
 
 # ~~~~~~~~~
 # Count times each model is in each rank
-Cnt<-apply(rnks,2,function(x){table(factor(x,levels=1:ncol(rnks)))})
+Cnt<-apply(rnks.RMSD,2,function(x){table(factor(x,levels=1:ncol(rnks.RMSD)))})
 Cnt
 pCnt <- round(prop.table(Cnt,2)*100,1)
 pCnt
@@ -169,24 +169,84 @@ setwd(wd)
 # ~~~~~~~~~
 
 # Of the times that AA is ranked first, how often are BD and CM  within X RMSD units?
-cnt<-apply(delV[rnks[,ncol(rnks)]==1,3:4] < delcutoff, 2, sum)
+cnt<-apply(delV.RMSD[rnks.RMSD[,ncol(rnks.RMSD)]==1,3:4] < cut.RMSD, 2, sum)
 cnt
 Cnt[1,'AA']
 cnt/Cnt[1,'AA']
 # Concl: BD and CM are within cutoff 60% of the time.
 
 # Of times that AA is ranked first, how often are *either* BD and CM within X RMSD units?
-cnt<-sum(apply(delV[rnks[,ncol(rnks)]==1,3:4] < delcutoff, 1, sum)>0)
+cnt<-sum(apply(delV.RMSD[rnks.RMSD[,ncol(rnks.RMSD)]==1,3:4] < cut.RMSD, 1, sum)>0)
 cnt
 cnt/Cnt[1,'AA']
 # Concl: BD or CM are within cutoff ~76% of the time
 
 # ~~~~~~~~~
 # What about for datasets that have a sample size of at least X?
+SScuts <- seq(5,300,by=1)
+fFirst.RMSD<-fSecnd.RMSD<-dim(0)
+for(SScut in SScuts){
+  Cnt<-apply(rnks.RMSD[sample.sizes>=SScut,],2,function(x){table(factor(x,levels=1:ncol(rnks.RMSD)))})
+  pCnt <- prop.table(Cnt,2)
+  fFirst.RMSD <- rbind(fFirst.RMSD,pCnt[1,])
+  fSecnd.RMSD <- rbind(fSecnd.RMSD,pCnt[2,])
+}
+
+# Darken colors of linear models
+Mcols[c(1,5)]<-'grey40'
+ltys <- c(1,1,1,6,2,2,3,1)
+pdf('../../../../results/R/OnePredOnePrey_figs/OnePredOnePrey_RMSD_rankBySS.pdf',
+    height=4,width=3)
+par(mar=c(2,6.5,4,0.5), mgp=c(1.5,0.2,0), tcl=-0.1, las=1, cex=0.8)
+layout(matrix(c(1,2,3),ncol=1), heights=c(1,3,3)) 
+par(mar=c(0,3,0,0))
+plot(1,1,type="n", axes=F, xlab="", ylab="")
+
+xadj<-0.7
+yadj<-1.2
+lcex<-0.7
+
+mods<-c(1,5)
+legend(xadj,yadj,legend=colnames(fFirst.RMSD)[mods],
+       lty=ltys[mods], col=Mcols[mods],lwd=1.5,
+       cex=lcex, ncol=1, pt.lwd=0.8, 
+       title=expression(italic(k)==1), bty='n')
+
+mods<-c(2,6,7)
+legend(xadj+0.2,yadj,legend=colnames(fFirst.RMSD)[mods],
+       lty=ltys[mods], col=Mcols[mods],lwd=1.5,
+       pt.cex=1.1,cex=lcex, ncol=1, pt.lwd=0.8,
+       title=expression(italic(k)==2), bty='n')
+
+mods<-c(3,4,8)
+legend(xadj+0.4,yadj,legend=colnames(fFirst.RMSD)[mods],
+       lty=ltys[mods], col=Mcols[mods],lwd=1.5,
+       pt.cex=1.1,cex=lcex, ncol=1, pt.lwd=0.8,
+       title=expression(italic(k)==3), bty='n')
+
+text(xadj+0.3, yadj+0.1, 'Models', adj=0.5, cex=1)
+
+
+par(mar=c(3,3,0.5,1))
+matplot(SScuts,fFirst.RMSD,las=1,type='l', col=Mcols,lty=ltys,cex=0.5,
+        ylim=c(0,0.8),lwd=1.5,
+        ylab='Fraction in first rank',
+        xlab='Sample size greater than...')
+legend('topleft',legend='A',inset=-0.05,bty='n',cex=1.2)
+matplot(SScuts,fSecnd.RMSD,las=1,type='l', col=Mcols,lty=ltys,cex=0.5,
+        ylim=c(0,0.4),lwd=1.5,
+        ylab='Fraction in second rank',
+        xlab='Sample size greater than...')
+legend('topleft',legend='B',inset=-0.05,bty='n',cex=1.2)
+dev.off()
+
+
+
 SScut <- 50
-Cnt<-apply(rnks[sample.sizes>=SScut,],2,function(x){table(factor(x,levels=1:ncol(rnks)))})
+Cnt<-apply(rnks.RMSD[sample.sizes>=SScut,],2,function(x){table(factor(x,levels=1:ncol(rnks.RMSD)))})
 Cnt
-pCnt <- round(prop.table(Cnt,2)*100,1)
+sum(Cnt[,1])
+pCnt <- prop.table(Cnt,2)
 pCnt
 
 # Either pass counts or combine counts and proportions before exporting table
