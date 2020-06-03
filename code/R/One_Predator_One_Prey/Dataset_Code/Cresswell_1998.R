cb061022ab8df9e1ecae37612875db80f6f8a344
Mark Novak <mnovak.m@gmail.com>
2020-06-02 19:25:56 -0700

    Fix Cresswell name

--- code/R/One_Predator_One_Prey/Dataset_Code/Cresswell_1998.R
+++ code/R/One_Predator_One_Prey/Dataset_Code/Cresswell_1998.R
@@ -1,7 +1,7 @@
 
 # read in the simplify the raw data
-datadir <- 'Creswell_1998'
-filename <- 'Creswell_1998.csv'
+datadir <- 'Cresswell_1998'
+filename <- 'Cresswell_1998.csv'
 rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
 
 # rename to standard column names used in fitting code
