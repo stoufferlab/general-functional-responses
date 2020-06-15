73a424d1e1f38f52b9a5fd318352ca481c5db7aa
Mark Novak <mnovak.m@gmail.com>
2020-06-15 07:27:32 -0700

    FIM before retry

--- code/Mathematica/FisherMatrix-1_Compute-HPC.m
+++ code/Mathematica/FisherMatrix-1_Compute-HPC.m
@@ -39,65 +39,50 @@ PoislL[func_]:=-n func+Log[func] x/.{n->1}
 (* ::Input::Initialization:: *)
 lLH1Poiss[a_]:=PoislL[H1]
 FisherH1Poiss=fisher[lLH1Poiss,{a}];
-
 EFisherH1Poiss=EFisher[FisherH1Poiss,H1]/.subs;
-
 DetH1=Det[EFisherH1Poiss];
 DetH1Trunc=DetEFisherTrunc[FisherH1Poiss,H1,0,Max[\[CapitalNu]vals],subs];
 
 lLRPoiss[a_]:=PoislL[R]
 FisherRPoiss=fisher[lLRPoiss,{a}]//FullSimplify;
-
 EFisherRPoiss=EFisher[FisherRPoiss,R]//.subs;
-
 DetR=Det[EFisherRPoiss]//FullSimplify;
 DetRTrunc=DetEFisherTrunc[FisherRPoiss,R,0,Max[\[CapitalNu]vals],subs];
 
 lLHVPoiss[a_,m_]:=PoislL[HV]
 FisherHVPoiss=fisher[lLHVPoiss,{a,m}]//FullSimplify;
-
 EFisherHVPoiss=EFisher[FisherHVPoiss,HV]//.subs;
-
 DetHV=Det[EFisherHVPoiss];
 DetHVTrunc=DetEFisherTrunc[FisherHVPoiss,HV,0,Max[\[CapitalNu]vals],subs];
 
 lLH2Poiss[a_,h_]:=PoislL[H2]
 FisherH2Poiss=fisher[lLH2Poiss,{a, h}];
-
 EFisherH2Poiss=EFisher[FisherH2Poiss,H2]//.subs;
-
 DetH2=Det[EFisherH2Poiss]//FullSimplify;
 DetH2Trunc=DetEFisherTrunc[FisherH2Poiss,H2,0,Max[\[CapitalNu]vals],subs];
 
 lLAGPoiss[a_,h_]:=PoislL[AG]
 FisherAGPoiss=fisher[lLAGPoiss,{a, h}]//FullSimplify;
-
 EFisherAGPoiss=EFisher[FisherAGPoiss,AG]//.subs;
-
 DetAG=Det[EFisherAGPoiss];
 DetAGTrunc=DetEFisherTrunc[FisherAGPoiss,AG,0,Max[\[CapitalNu]vals],subs];
 
-lLCMPoiss[a_,h_,\[Gamma]_]:=PoislL[CM]
-FisherCMPoiss=fisher[lLCMPoiss,{a, h,\[Gamma]}]//FullSimplify;
-
-EFisherCMPoiss=EFisher[FisherCMPoiss,CM]//.subs;
-
-DetCM=Det[EFisherCMPoiss];
-DetCMTrunc=DetEFisherTrunc[FisherCMPoiss,CM,0,Max[\[CapitalNu]vals],subs];
-
 lLBDPoiss[a_,h_,\[Gamma]_]:=PoislL[BD]
 FisherBDPoiss=fisher[lLBDPoiss,{a, h,\[Gamma]}]//FullSimplify;
-
 EFisherBDPoiss=EFisher[FisherBDPoiss,BD]//.subs;
-
 DetBD=Det[EFisherBDPoiss];
 DetBDTrunc=DetEFisherTrunc[FisherBDPoiss,BD,0,Max[\[CapitalNu]vals],subs];
 
+
+lLCMPoiss[a_,h_,\[Gamma]_]:=PoislL[CM]
+FisherCMPoiss=fisher[lLCMPoiss,{a, h,\[Gamma]}]//FullSimplify;
+EFisherCMPoiss=EFisher[FisherCMPoiss,CM]//.subs;
+DetCM=Det[EFisherCMPoiss];
+DetCMTrunc=DetEFisherTrunc[FisherCMPoiss,CM,0,Max[\[CapitalNu]vals],subs];
+
 lLAAPoiss[a_,h_,m_]:=PoislL[AA]
 FisherAAPoiss=fisher[lLAAPoiss,{a, h,m}]//FullSimplify;
-
 EFisherAAPoiss=EFisher[FisherAAPoiss,AA]//.subs;
-
 DetAA=Det[EFisherAAPoiss];
 DetAATrunc=DetEFisherTrunc[FisherAAPoiss,AA,0,Max[\[CapitalNu]vals],subs];
 
@@ -105,118 +90,124 @@ Save["FisherMatrix-1_Output_k1k2k3","`*"];
 
 
 (* ::Input::Initialization:: *)
+NIntMethod= "LocalAdaptive";
 accgoal=3;
+maxRec=500;
+P\[CapitalNu]prods=Sort[DeleteDuplicates[Join@@Outer[Times,Pvals,\[CapitalNu]vals]]];
 P\[CapitalNu]rats=Sort[DeleteDuplicates[Join@@Outer[Divide,Pvals,\[CapitalNu]vals]]];
 e=10;
 ParmRangeA={
-{a,0,1000},
-DeleteDuplicates[Flatten[{{h},Sort[{0,P\[CapitalNu]rats,1,10^e},Less]}]],
-DeleteDuplicates[Flatten[{{\[Gamma]},Sort[{0,1/Pvals,1,10^e},Less]}]],
-DeleteDuplicates[Flatten[{{m},Sort[{0,P\[CapitalNu]rats,1,5},Less]}]]
+DeleteDuplicates[Flatten[{{a},Sort[{0,P\[CapitalNu]rats,Max[\[CapitalNu]vals]/P\[CapitalNu]prods,1,10^e},Less]}]],
+DeleteDuplicates[Flatten[{{h},Sort[{0,P\[CapitalNu]rats,Max[\[CapitalNu]vals]/P\[CapitalNu]prods,1,10^e},Less]}]],
+DeleteDuplicates[Flatten[{{\[Gamma]},Sort[{0,1/Pvals,Max[\[CapitalNu]vals]/P\[CapitalNu]prods,1,10^e},Less]}]],
+DeleteDuplicates[Flatten[{{m},Sort[{0,P\[CapitalNu]rats,Max[\[CapitalNu]vals]/P\[CapitalNu]prods,1,10^e},Less]}]]
 };
 
 NIntH1=Log[NIntegrate[Sqrt[DetH1],
 ParmRangeA[[1]],
-AccuracyGoal->accgoal]]
-
+AccuracyGoal->accgoal,
+Method->NIntMethod,
+MaxRecursion->maxRec]]
 NIntH1Trunc=Log[NIntegrate[Sqrt[DetH1Trunc],
 ParmRangeA[[1]],
-AccuracyGoal->accgoal]]
+AccuracyGoal->accgoal,
+Method->NIntMethod,
+MaxRecursion->maxRec]]
 
 NIntR=Log[NIntegrate[Sqrt[DetR],
 ParmRangeA[[1]],
-AccuracyGoal->accgoal]]
-
+AccuracyGoal->accgoal,
+Method->NIntMethod,
+MaxRecursion->maxRec]]
 NIntRTrunc=Log[NIntegrate[Sqrt[DetRTrunc],
 ParmRangeA[[1]],
-AccuracyGoal->accgoal]]
+AccuracyGoal->accgoal,
+Method->NIntMethod,
+MaxRecursion->maxRec]]
 
 NIntHV=Log[NIntegrate[Sqrt[DetHV],
 ParmRangeA[[1]],
 ParmRangeA[[4]],
-AccuracyGoal->accgoal]]
-
+AccuracyGoal->accgoal,
+Method->NIntMethod,
+MaxRecursion->maxRec]]
 NIntHVTrunc=Log[NIntegrate[Sqrt[DetHVTrunc],
 ParmRangeA[[1]],
 ParmRangeA[[4]],
 AccuracyGoal->accgoal,
-Method->{"LocalAdaptive"},
-MaxRecursion->500]]
+Method->NIntMethod,
+MaxRecursion->maxRec]]
 
 NIntH2=Log[NIntegrate[Sqrt[DetH2],
 ParmRangeA[[1]],
 ParmRangeA[[2]],
 AccuracyGoal->accgoal,
-Method->{"LocalAdaptive"},
-MaxRecursion->500]]
-
+Method->NIntMethod,
+MaxRecursion->maxRec]]
 NIntH2Trunc=Log[NIntegrate[Sqrt[DetH2Trunc],
 ParmRangeA[[1]],
 ParmRangeA[[2]],
 AccuracyGoal->accgoal,
-Method->{"LocalAdaptive"},
-MaxRecursion->500]]
+Method->NIntMethod,
+MaxRecursion->maxRec]]
 
 NIntAG=Log[NIntegrate[Sqrt[DetAG],
 ParmRangeA[[1]],
 ParmRangeA[[2]],
 AccuracyGoal->accgoal,
-Method->{"LocalAdaptive"},
-MaxRecursion->500]]
-
+Method->NIntMethod,
+MaxRecursion->maxRec]]
 NIntAGTrunc=Log[NIntegrate[Sqrt[DetAGTrunc],
 ParmRangeA[[1]],
 ParmRangeA[[2]],
 AccuracyGoal->accgoal,
-Method->{"LocalAdaptive"},
-MaxRecursion->500]]
+Method->NIntMethod,
+MaxRecursion->maxRec]]
 
 NIntBD=Log[NIntegrate[Sqrt[DetBD],
 ParmRangeA[[1]],
 ParmRangeA[[2]],
 ParmRangeA[[3]],
 AccuracyGoal->accgoal,
-Method->{"LocalAdaptive"},
-MaxRecursion->500]]
-
+Method->NIntMethod,
+MaxRecursion->maxRec]]
 NIntBDTrunc=Log[NIntegrate[Sqrt[DetBDTrunc],
 ParmRangeA[[1]],
 ParmRangeA[[2]],
 ParmRangeA[[3]],
 AccuracyGoal->accgoal,
-Method->{"LocalAdaptive"},
-MaxRecursion->500]]
+Method->NIntMethod,
+MaxRecursion->maxRec]]
 
 NIntCM=Log[NIntegrate[Sqrt[DetCM],
 ParmRangeA[[1]],
 ParmRangeA[[2]],
 ParmRangeA[[3]],
 AccuracyGoal->accgoal,
-Method->{"LocalAdaptive"},
-MaxRecursion->500]]
-
+Method->NIntMethod,
+MaxRecursion->maxRec]]
 NIntCMTrunc=Log[NIntegrate[Sqrt[DetCMTrunc],
 ParmRangeA[[1]],
 ParmRangeA[[2]],
 ParmRangeA[[3]],
 AccuracyGoal->accgoal,
-Method->{"LocalAdaptive"},
-MaxRecursion->500]]
+Method->NIntMethod,
+MaxRecursion->maxRec]]
 
 NIntAA=Log[NIntegrate[Sqrt[DetAA],
 ParmRangeA[[1]],
 ParmRangeA[[2]],
 ParmRangeA[[4]],
 AccuracyGoal->accgoal,
-MaxRecursion->500]]
-
+Method->NIntMethod,
+MaxRecursion->maxRec]]
 NIntAATrunc=Log[NIntegrate[Sqrt[DetAATrunc],
 ParmRangeA[[1]],
 ParmRangeA[[2]],
 ParmRangeA[[4]],
 AccuracyGoal->accgoal,
-Method->{"LocalAdaptive"},
-MaxRecursion->500]]
+Method->NIntMethod,
+MaxRecursion->maxRec]]
 
 Save["FisherMatrix-1_Output_k1k2k3_NInt","`*"];
 
@@ -232,50 +223,74 @@ aNIntRTrunc=Sqrt[DetRTrunc];
 
 aNIntHV=NIntegrate[Sqrt[DetHV],
 ParmRangeB[[3]],
-AccuracyGoal->accgoal];
+AccuracyGoal->accgoal,
+Method->NIntMethod,
+MaxRecursion->maxRec];
 aNIntHVTrunc[\[Alpha]_?NumericQ]:=NIntegrate[Sqrt[DetHVTrunc/.{a->\[Alpha]}],
 ParmRangeB[[3]],
-AccuracyGoal->accgoal];
+AccuracyGoal->accgoal,
+Method->NIntMethod,
+MaxRecursion->maxRec];
 
 aNIntH2[\[Alpha]_?NumericQ]:=NIntegrate[Sqrt[DetH2/.{a->\[Alpha]}],
 ParmRangeB[[1]],
-AccuracyGoal->accgoal];
+AccuracyGoal->accgoal,
+Method->NIntMethod,
+MaxRecursion->maxRec];
 aNIntH2Trunc[\[Alpha]_?NumericQ]:=NIntegrate[Sqrt[DetH2Trunc/.{a->\[Alpha]}],
 ParmRangeB[[1]],
-AccuracyGoal->accgoal];
+AccuracyGoal->accgoal,
+Method->NIntMethod,
+MaxRecursion->maxRec];
 
 aNIntAG[\[Alpha]_?NumericQ]:=NIntegrate[Sqrt[DetAG/.{a->\[Alpha]}],
 ParmRangeB[[1]],
-AccuracyGoal->accgoal];
+AccuracyGoal->accgoal,
+Method->NIntMethod,
+MaxRecursion->maxRec];
 aNIntAGTrunc[\[Alpha]_?NumericQ]:=NIntegrate[Sqrt[DetAGTrunc/.{a->\[Alpha]}],
 ParmRangeB[[1]],
-AccuracyGoal->accgoal];
+AccuracyGoal->accgoal,
+Method->NIntMethod,
+MaxRecursion->maxRec];
 
 aNIntBD[\[Alpha]_?NumericQ]:=NIntegrate[Sqrt[DetBD/.{a->\[Alpha]}],
 ParmRangeB[[1]],
 ParmRangeB[[2]],
-AccuracyGoal->accgoal];
+AccuracyGoal->accgoal,
+Method->NIntMethod,
+MaxRecursion->maxRec];
 aNIntBDTrunc[\[Alpha]_?NumericQ]:=NIntegrate[Sqrt[DetBDTrunc/.{a->\[Alpha]}],
 ParmRangeB[[1]],
 ParmRangeB[[2]],
-AccuracyGoal->accgoal];
+AccuracyGoal->accgoal,
+Method->NIntMethod,
+MaxRecursion->maxRec];
 
 aNIntCM[\[Alpha]_?NumericQ]:=NIntegrate[Sqrt[DetCM/.{a->\[Alpha]}],
 ParmRangeB[[1]],
 ParmRangeB[[2]],
-AccuracyGoal->accgoal];
+AccuracyGoal->accgoal,
+Method->NIntMethod,
+MaxRecursion->maxRec];
 aNIntCMTrunc[\[Alpha]_?NumericQ]:=NIntegrate[Sqrt[DetCMTrunc/.{a->\[Alpha]}],
 ParmRangeB[[1]],
 ParmRangeB[[2]],
-AccuracyGoal->accgoal];
+AccuracyGoal->accgoal,
+Method->NIntMethod,
+MaxRecursion->maxRec];
 
 aNIntAA[\[Alpha]_?NumericQ]:=NIntegrate[Sqrt[DetAA/.{a->\[Alpha]}],
 ParmRangeB[[1]],
 ParmRangeB[[3]],
-AccuracyGoal->accgoal];
+AccuracyGoal->accgoal,
+Method->NIntMethod,
+MaxRecursion->maxRec];
 aNIntAATrunc[\[Alpha]_?NumericQ]:=NIntegrate[Sqrt[DetAATrunc/.{a->\[Alpha]}],
 ParmRangeB[[1]],
 ParmRangeB[[3]],
-AccuracyGoal->accgoal];
+AccuracyGoal->accgoal,
+Method->NIntMethod,
+MaxRecursion->maxRec];
 
 Save["FisherMatrix-1_Output_k1k2k3_aNInt","`*"];
