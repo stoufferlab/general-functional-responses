f9dbeaa97bb1b5e6c7daf82bca4352e9d6f06d54
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2020-07-15 13:47:42 +1200

    I hate ifelse

--- code/R/lib/read_data.R
+++ code/R/lib/read_data.R
@@ -3,10 +3,10 @@
 # second reads the file from dropbox when no such file exists
 read.data <- function(datadir, filename, datatype, dropboxdir=""){
 	fname <- paste("../../../data", datatype, datadir, filename,sep="/")
-  	rawdata <- ifelse(
-  		file.exists(fname),
-  		read.csv(fname),
-  		read.csv(paste(dropboxdir,datadir,filename,sep="/"))
-  	)
+  	if(file.exists(fname)){
+  		rawdata <- read.csv(fname)
+  	}else{
+  		rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
+  	}
   	return(rawdata)
 }
