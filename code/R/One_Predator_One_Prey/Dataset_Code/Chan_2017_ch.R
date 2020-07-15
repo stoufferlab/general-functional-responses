d5ff446546263993f94ca87d478e54f8a681fd51
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2020-07-15 13:48:58 +1200

    use the common read.data function

--- code/R/One_Predator_One_Prey/Dataset_Code/Chan_2017_ch.R
+++ code/R/One_Predator_One_Prey/Dataset_Code/Chan_2017_ch.R
@@ -2,8 +2,4 @@
 # read in the simplify the raw data
 datadir <- 'Chan_2017'
 filename <- 'Chan_2017_ch.csv'
-rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
-
-# rename to standard column names used in fitting code
-d <- rawdata[,c("Coyotes.p.100km", "Hares.p.100km", "HareKills", "Coyote.Time.km")]
-colnames(d) <- c("Npredator", "Nprey", "Nconsumed", "Time")
+d <- read.data(datadir, filename, "One_Predator_One_Prey", dropboxdir)
