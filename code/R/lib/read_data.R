fd1d867d062ce04eb93f3ea65f790dce60e3e7c2
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2020-07-15 15:13:04 +1200

    if no such file exists, return data as NULL

--- code/R/lib/read_data.R
+++ code/R/lib/read_data.R
@@ -3,10 +3,15 @@
 # second reads the file from dropbox when no such file exists
 read.data <- function(datadir, filename, datatype, dropboxdir=""){
 	fname <- paste("../../../data", datatype, datadir, filename,sep="/")
-  	if(file.exists(fname)){
-  		rawdata <- read.csv(fname)
-  	}else{
-  		rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
-  	}
-  	return(rawdata)
+	if(file.exists(fname)){
+		rawdata <- read.csv(fname)
+	}else{
+		fname <- paste(dropboxdir,datadir,filename,sep="/")
+		if(file.exists(fname)){
+			rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
+		}else{
+			rawdata <- NULL
+		}
+	}
+	return(rawdata)
 }
