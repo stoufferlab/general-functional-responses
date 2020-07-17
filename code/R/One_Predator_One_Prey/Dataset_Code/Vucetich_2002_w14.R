0e7b6c4f3cb5117e9f81e375b43c795b00a13548
Mark Novak <mnovak.m@gmail.com>
2020-07-16 18:25:18 -0700

    Reverse additional changes to One-Prey Dataset_Code scripts (Commit: 9d8ac51 Jul 14, 2020 at 8:33 PM)
    These were missed in Commit 4a45234 Jul 16, 2020 at 6:19 PM

--- code/R/One_Predator_One_Prey/Dataset_Code/Vucetich_2002_w14.R
+++ code/R/One_Predator_One_Prey/Dataset_Code/Vucetich_2002_w14.R
@@ -2,10 +2,8 @@
 # read in the simplify the raw data
 datadir <- 'Vucetich_2002'
 filename <- 'IsleRoyale_Whole2014.csv'
-d <- read.data(datadir, filename, "One_Predator_One_Prey", dropboxdir)
+rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
 
 # rename to standard column names used in fitting code
-if(!is.null(d)){
-	d <- d[,c('Preds','Prey','TotalEatenPerObsTime','ObsTime')]
+d <- rawdata[,c('Preds','Prey','TotalEatenPerObsTime','ObsTime')]
 	colnames(d) <- c("Npredator", "Nprey", "Nconsumed", "Time")
-}
