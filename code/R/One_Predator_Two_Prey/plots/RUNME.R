ed38b5bf4d86a0b7f96626d5c5475e8c11fc659d
Daniel B. Stouffer <daniel.stouffer@canterbury.ac.nz>
2020-07-20 10:33:55 +1200

    making a sly note to the fact that we have a plotting script that doesn't actually work

--- code/R/One_Predator_Two_Prey/plots/RUNME.R
+++ code/R/One_Predator_Two_Prey/plots/RUNME.R
@@ -4,3 +4,7 @@ source('profile_phis.R')
 
 # generate the key 1pred2prey plot
 source('plot_AIC_phis.R')
+
+# NOTE: the below was never updated for the 1pred2prey data
+# # plot the observed consumptions rates relative to predicted values
+# source('plot_datasets_obs_V_fit.R')
