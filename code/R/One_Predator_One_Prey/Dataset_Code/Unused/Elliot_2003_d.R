c3586ede097ce0cb543d4ecba273854a4c045107
Mark Novak <mnovak.m@gmail.com>
2020-08-20 18:10:06 -0700

    Remove Unused dataset codes

--- code/R/One_Predator_One_Prey/Dataset_Code/Unused/Elliot_2003_d.R
+++
@@ -1,9 +0,0 @@
-
-# read in the simplify the raw data
-datadir <- 'Elliot_2003'
-filename <- 'Elliot_2003_d.csv'
-rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
-
-# turn into a standard dataframe with standard column names
-d <- rawdata[,c('Preds','Prey','PreyEatenTotal.Mean','PreyEatenTotal.SE','n', 'Time')]
-colnames(d) <- c("Npredator", "Nprey", "Nconsumed.mean", "Nconsumed.se", 'n', 'Time')
--- code/R/One_Predator_One_Prey/Dataset_Code/Unused/IsleRoyale_Mixed1998_Jost.R
+++
@@ -1,9 +0,0 @@
-
-# read in the simplify the raw data
-datadir <- 'Vucetich_2002'
-filename <- 'IsleRoyale_Mixed1998_Jost.csv'
-rawdata <- read.csv(paste(dropboxdir,datadir,filename,sep="/"))
-
-# rename to standard column names used in fitting code
-d <- rawdata[,c("Pred", "Prey", "TotalKillsPerDay")]
-colnames(d) <- c("Npredator", "Nprey", "Nconsumed")
